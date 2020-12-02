03a7a11f7266d7dbe29bdd7ffdc48bdd96839a9d
sugargoat <sugargoat@mobilecoin.com>
2020-12-01 23:12:54 -0800

    Account refactored

--- src/db.rs
+++ src/db.rs
@@ -125,47 +125,6 @@ impl WalletDb {
         Ok(Self::new(pool, logger))
     }
 
-    /// Get a specific account
-    pub fn get_account(&self, account_id_hex: &str) -> Result<Account, WalletDbError> {
-        let conn = self.pool.get()?;
-
-        match dsl_accounts
-            .find(account_id_hex)
-            .get_result::<Account>(&conn)
-        {
-            Ok(a) => Ok(a),
-            // Match on NotFound to get a more informative NotFound Error
-            Err(diesel::result::Error::NotFound) => {
-                Err(WalletDbError::NotFound(account_id_hex.to_string()))
-            }
-            Err(e) => Err(e.into()),
-        }
-    }
-
-    /// Update an account.
-    /// The only updatable field is the name. Any other desired update requires adding
-    /// a new account, and deleting the existing if desired.
-    pub fn update_account_name(
-        &self,
-        account_id_hex: &str,
-        new_name: String,
-    ) -> Result<(), WalletDbError> {
-        let conn = self.pool.get()?;
-
-        diesel::update(dsl_accounts.find(account_id_hex))
-            .set(schema_accounts::name.eq(new_name))
-            .execute(&conn)?;
-        Ok(())
-    }
-
-    /// Delete an account.
-    pub fn delete_account(&self, account_id_hex: &str) -> Result<(), WalletDbError> {
-        let conn = self.pool.get()?;
-
-        diesel::delete(dsl_accounts.find(account_id_hex)).execute(&conn)?;
-        Ok(())
-    }
-
     pub fn create_assigned_subaddress(
         &self,
         account_id_hex: &str,
@@ -1169,9 +1128,7 @@ impl WalletDb {
 mod tests {
     use super::*;
     use crate::db_models::account::AccountModel;
-    use crate::db_models::assigned_subaddress::AssignedSubaddressModel;
     use crate::test_utils::WalletDbTestContext;
-    use mc_account_keys::RootIdentity;
     use mc_common::logger::{test_with_logger, Logger};
     use mc_crypto_keys::{RistrettoPrivate, RistrettoPublic};
     use mc_transaction_core::encrypted_fog_hint::EncryptedFogHint;
@@ -1179,122 +1136,14 @@ mod tests {
     use mc_transaction_core::ring_signature::KeyImage;
     use mc_util_from_random::FromRandom;
     use rand::{rngs::StdRng, SeedableRng};
-    use std::collections::HashSet;
     use std::convert::TryFrom;
-    use std::iter::FromIterator;
-
-    #[test_with_logger]
-    fn test_account_crud(logger: Logger) {
-        let mut rng: StdRng = SeedableRng::from_seed([20u8; 32]);
-
-        let db_test_context = WalletDbTestContext::default();
-        let wallet_db = db_test_context.get_db_instance(logger);
-
-        let account_key = AccountKey::random(&mut rng);
-        let account_id_hex = {
-            let conn = wallet_db.get_conn().unwrap();
-            let (account_id_hex, _public_address_b58) =
-                Account::create(&account_key, 0, 1, 2, 0, 1, "Alice's Main Account", &conn)
-                    .unwrap();
-            account_id_hex
-        };
-
-        let res = Account::list_accounts(&wallet_db.get_conn().unwrap()).unwrap();
-        assert_eq!(res.len(), 1);
-
-        let acc = wallet_db.get_account(&account_id_hex).unwrap();
-        let expected_account = Account {
-            account_id_hex: account_id_hex.clone(),
-            encrypted_account_key: mc_util_serial::encode(&account_key),
-            main_subaddress_index: 0,
-            change_subaddress_index: 1,
-            next_subaddress_index: 2,
-            first_block: 0,
-            next_block: 1,
-            name: "Alice's Main Account".to_string(),
-        };
-        assert_eq!(expected_account, acc);
-
-        // Verify that the subaddress table entries were updated for main and change
-        let subaddresses = wallet_db.list_subaddresses(&account_id_hex).unwrap();
-        assert_eq!(subaddresses.len(), 2);
-        let subaddress_indices: HashSet<i64> =
-            HashSet::from_iter(subaddresses.iter().map(|s| s.subaddress_index));
-        assert!(subaddress_indices.get(&0).is_some());
-        assert!(subaddress_indices.get(&1).is_some());
-
-        // Verify that we can get the correct subaddress index from the spend public key
-        let main_subaddress = account_key.subaddress(0);
-        let (retrieved_index, retrieved_acocunt_id_hex) =
-            AssignedSubaddress::find_by_subaddress_spend_public_key(
-                main_subaddress.spend_public_key(),
-                &wallet_db.get_conn().unwrap(),
-            )
-            .unwrap();
-        assert_eq!(retrieved_index, 0);
-        assert_eq!(retrieved_acocunt_id_hex, account_id_hex);
-
-        // Add another account with no name, scanning from later
-        let account_key_secondary = AccountKey::from(&RootIdentity::from_random(&mut rng));
-        let (account_id_hex_secondary, _public_address_b58_secondary) = Account::create(
-            &account_key_secondary,
-            0,
-            1,
-            2,
-            50,
-            51,
-            "",
-            &wallet_db.get_conn().unwrap(),
-        )
-        .unwrap();
-        let res = Account::list_accounts(&wallet_db.get_conn().unwrap()).unwrap();
-        assert_eq!(res.len(), 2);
-
-        let acc_secondary = wallet_db.get_account(&account_id_hex_secondary).unwrap();
-        let mut expected_account_secondary = Account {
-            account_id_hex: account_id_hex_secondary.clone(),
-            encrypted_account_key: mc_util_serial::encode(&account_key_secondary),
-            main_subaddress_index: 0,
-            change_subaddress_index: 1,
-            next_subaddress_index: 2,
-            first_block: 50,
-            next_block: 51,
-            name: "".to_string(),
-        };
-        assert_eq!(expected_account_secondary, acc_secondary);
-
-        // Update the name for the secondary account
-        wallet_db
-            .update_account_name(
-                &account_id_hex_secondary,
-                "Alice's Secondary Account".to_string(),
-            )
-            .unwrap();
-        let acc_secondary2 = wallet_db.get_account(&account_id_hex_secondary).unwrap();
-        expected_account_secondary.name = "Alice's Secondary Account".to_string();
-        assert_eq!(expected_account_secondary, acc_secondary2);
-
-        // Delete the secondary account
-        wallet_db.delete_account(&account_id_hex_secondary).unwrap();
-
-        let res = Account::list_accounts(&wallet_db.get_conn().unwrap()).unwrap();
-        assert_eq!(res.len(), 1);
-
-        // Attempt to get the deleted account
-        let res = wallet_db.get_account(&account_id_hex_secondary);
-        match res {
-            Ok(_) => panic!("Should have deleted account"),
-            Err(WalletDbError::NotFound(s)) => assert_eq!(s, account_id_hex_secondary.to_string()),
-            Err(_) => panic!("Should error with NotFound but got {:?}", res),
-        }
-    }
 
     #[test_with_logger]
     fn test_received_tx_lifecycle(logger: Logger) {
         let mut rng: StdRng = SeedableRng::from_seed([20u8; 32]);
 
         let db_test_context = WalletDbTestContext::default();
-        let walletdb = db_test_context.get_db_instance(logger);
+        let wallet_db = db_test_context.get_db_instance(logger);
 
         let account_key = AccountKey::random(&mut rng);
         let (account_id_hex, _public_address_b58) = Account::create(
@@ -1305,7 +1154,7 @@ mod tests {
             0,
             1,
             "Alice's Main Account",
-            &walletdb.get_conn().unwrap(),
+            &wallet_db.get_conn().unwrap(),
         )
         .unwrap();
 
@@ -1333,7 +1182,7 @@ mod tests {
 
         let received_block_height = 144;
 
-        let txo_hex = walletdb
+        let txo_hex = wallet_db
             .create_received_txo(
                 txo.clone(),
                 Some(subaddress_index),
@@ -1344,7 +1193,7 @@ mod tests {
             )
             .unwrap();
 
-        let txos = walletdb.list_txos(&account_id_hex).unwrap();
+        let txos = wallet_db.list_txos(&account_id_hex).unwrap();
         assert_eq!(txos.len(), 1);
 
         let expected_txo = Txo {
@@ -1372,7 +1221,7 @@ mod tests {
         assert_eq!(txos[0].1, expected_txo_status);
 
         // Verify that the status filter works as well
-        let balances = walletdb.list_txos_by_status(&account_id_hex).unwrap();
+        let balances = wallet_db.list_txos_by_status(&account_id_hex).unwrap();
         assert_eq!(balances["unspent"].len(), 1);
 
         // Now we'll "spend" the TXO
@@ -1382,7 +1231,7 @@ mod tests {
         // Now we'll process the ledger and verify that the TXO was spent
         let spent_block_height = 365;
 
-        walletdb
+        wallet_db
             .update_spent_and_increment_next_block(
                 &account_id_hex,
                 spent_block_height,
@@ -1390,7 +1239,7 @@ mod tests {
             )
             .unwrap();
 
-        let txos = walletdb.list_txos(&account_id_hex).unwrap();
+        let txos = wallet_db.list_txos(&account_id_hex).unwrap();
         assert_eq!(txos.len(), 1);
         assert_eq!(
             txos[0].0.spent_block_height.unwrap(),
@@ -1399,11 +1248,11 @@ mod tests {
         assert_eq!(txos[0].1.txo_status, "spent".to_string());
 
         // Verify that the next block height is + 1
-        let account = walletdb.get_account(&account_id_hex).unwrap();
+        let account = Account::get(&account_id_hex, &wallet_db.get_conn().unwrap()).unwrap();
         assert_eq!(account.next_block, spent_block_height + 1);
 
         // Verify that there are no unspent txos
-        let balances = walletdb.list_txos_by_status(&account_id_hex).unwrap();
+        let balances = wallet_db.list_txos_by_status(&account_id_hex).unwrap();
         assert!(balances["unspent"].is_empty());
     }
 }
