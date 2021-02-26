3be3ec6c9d87ecc50f9e5583d8af1af45e6310ae
mfaulk <mfaulk@users.noreply.github.com>
2021-02-25 16:39:39 -0800

    Update full-service/src/db/models.rs
    Co-authored-by: sugargoat <sara.drakeley@mobilecoin.com>

--- full-service/src/db/models.rs
+++ full-service/src/db/models.rs
@@ -59,7 +59,7 @@ pub struct Account {
     pub next_subaddress_index: i64,
     /// Index of the first block where this account may have held funds.
     pub first_block: i64,
-    /// TODO: ???
+    /// The next block to be scanned for syncing the account.
     pub next_block: i64,
     /// If the account was imported, account history prior to this block index
     /// is derived from the public ledger, and does not reflect client-side
