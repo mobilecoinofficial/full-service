c2ca3b972880502ed0a06d4fb2382ce52651b41d
Christian Oudard <christian.oudard@gmail.com>
2021-03-30 15:38:43 -0700

    Improve naming of key derivation versions.
    Co-authored-by: Kyle Fleming <kyle@kylefleming.net>

--- full-service/src/db/account.rs
+++ full-service/src/db/account.rs
@@ -30,8 +30,8 @@ pub const DEFAULT_CHANGE_SUBADDRESS_INDEX: u64 = 1;
 pub const DEFAULT_NEXT_SUBADDRESS_INDEX: u64 = 2;
 pub const DEFAULT_FIRST_BLOCK_INDEX: u64 = 0;
 
-pub const KEY_DERIVATION_FROM_ROOT_ENTROPY: u8 = 1;
-pub const KEY_DERIVATION_FROM_MNEMONIC: u8 = 2;
+pub const ROOT_ENTROPY_KEY_DERIVATION_VERSION: u8 = 1;
+pub const MNEMONIC_KEY_DERIVATION_VERSION: u8 = 2;
 
 #[derive(Debug, Clone, Eq, PartialEq, Hash)]
 pub struct AccountID(pub String);
