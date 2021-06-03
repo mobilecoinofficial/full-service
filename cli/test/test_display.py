ed118a297c0eece74ad29ff7f8d4eb0d2b8b27a0
Christian Oudard <christian.oudard@gmail.com>
2021-06-03 16:30:12 -0700

    Update messages to use _format_mob universally. Add test cases showing removal of trailing zeros.

--- cli/test/test_display.py
+++ cli/test/test_display.py
@@ -5,7 +5,11 @@ def test_format_decimal():
     f = lambda x: str(_format_decimal(x))
     assert f('4200') == '4200'
     assert f('42') == '42'
+    assert f('42.0') == '42'
+    assert f('42.0000') == '42'
     assert f('4.2') == '4.2'
+    assert f('4.20') == '4.2'
     assert f('.42') == '0.42'
     assert f('.0042') == '0.0042'
+    assert f('.004200') == '0.0042'
     assert f('.00000042') == '0.00000042'
