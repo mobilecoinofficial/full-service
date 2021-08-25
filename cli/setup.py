bebadbc8e9462abcd725b6f84ee1b8094057e071
Christian Oudard <christian.oudard@gmail.com>
2021-08-24 19:14:15 -0700

    Clean up default full-service executable names. Add env var script and install script to setup.py.

--- cli/setup.py
+++ cli/setup.py
@@ -3,8 +3,8 @@
 from distutils.core import setup
 
 setup(
-    name='mobilecoin',
-    version='0.1',
+    name='mobilecoin-cli',
+    version='1.0',
     description='Command line client for MobileCoin.',
     author='Christian Oudard',
     author_email='christian@christianoudard.com',
@@ -12,4 +12,7 @@ setup(
     packages=['mobilecoin'],
     scripts=['bin/mobcli'],
     install_requires=[],
+    data_files=[
+        ('scripts', ['mc_env.sh', 'install.sh']),
+    ],
 )
