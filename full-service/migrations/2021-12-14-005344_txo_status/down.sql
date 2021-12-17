96f5abde21b9c6336c885c24531a5b3fc9021fda
Christian Oudard <christian.oudard@gmail.com>
2021-12-16 17:24:56 -0800

    WIP data migrations.

--- full-service/migrations/2021-12-14-005344_txo_status/down.sql
+++ full-service/migrations/2021-12-14-005344_txo_status/down.sql
@@ -1,5 +1,15 @@
--- todo: add accounttxostatus
--- todo: data migration
+CREATE TABLE account_txo_statuses (
+      account_id_hex TEXT NOT NULL,
+      txo_id_hex TEXT NOT NULL,
+      txo_status TEXT NOT NULL,
+      txo_type TEXT NOT NULL,
+      PRIMARY KEY (account_id_hex, txo_id_hex),
+      FOREIGN KEY (account_id_hex) REFERENCES accounts(account_id_hex),
+      FOREIGN KEY (txo_id_hex) REFERENCES txos(txo_id_hex)
+);
+
+-- todo: reverse data migration
+
 ALTER TABLE txos REMOVE COLUMN minted_account_id_hex;
 ALTER TABLE txos REMOVE COLUMN received_account_id_hex;
 
