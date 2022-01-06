0552b8f741e356f181d2655e7cfc2d35ec7e8c3c
wjuan-mob <william@mobilecoin.com>
2022-01-06 11:31:15 -0500

    Adding server stub

--- validator-service/src/lib.rs
+++ validator-service/src/lib.rs
@@ -1,5 +1,6 @@
 // Copyright 2018-2021 MobileCoin, Inc.
 
 pub mod config;
+pub mod server;
 
-pub use crate::{config::Config};
+pub use crate::{config::Config, server::Server};
---
+++ validator-service/src/server.rs
@@ -0,0 +1,73 @@
+use futures::executor::block_on;
+use grpcio::{Server as GrpcioServer, ServerBuilder};
+use mc_common::{
+    logger::{log, Logger},
+};
+use std::{
+    sync::{Arc},
+};
+use mc_ledger_sync::{LedgerSyncServiceThread};
+
+const NETWORK: &str = "test";
+
+/// The application server
+pub struct Server {
+    server: GrpcioServer,
+    ledger_sync: Option<LedgerSyncServiceThread>,
+    logger: Logger,
+}
+
+impl Server {
+    pub fn new(
+        logger: Logger,
+    ) -> Self {
+        //TODO update name
+        log::info!(logger, "starting, network = {}", NETWORK);
+
+        let env = Arc::new(
+            grpcio::EnvBuilder::new()
+                .name_prefix("ValidatorNode-RPC".to_string())
+                .build(),
+        );
+
+        let server_builder = ServerBuilder::new(env);
+        log::info!(logger, "Registered service");
+
+        let server = server_builder.build().unwrap();
+        Self {
+            server,
+            ledger_sync: None,
+            logger,
+        }
+    }
+
+    pub fn start(&mut self) {
+        self.server.start();
+        log::info!(
+            self.logger,
+            "Inside Server Start: Binding to {} host/ports",
+            self.server.bind_addrs().len()
+        );
+        for (host, port) in self.server.bind_addrs() {
+            log::info!(self.logger, "API listening on {}:{}", host, port);
+        }
+    }
+
+    pub fn stop(&mut self) {
+        block_on(self.server.shutdown()).expect("Could not stop grpc server");
+        match &mut self.ledger_sync {
+            // The division was valid
+            Some(ledger_sync) => {
+                ledger_sync.stop();
+            }
+            // The division was invalid
+            None => println!("Cannot divide by 0"),
+        }
+    }
+}
+
+impl Drop for Server {
+    fn drop(&mut self) {
+        self.stop();
+    }
+}
