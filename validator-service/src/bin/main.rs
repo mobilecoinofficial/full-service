415afe984a9067e427013365e7b58889c63935fb
wjuan-mob <william@mobilecoin.com>
2022-01-06 11:55:49 -0500

    Starting server in main

--- validator-service/src/bin/main.rs
+++ validator-service/src/bin/main.rs
@@ -2,7 +2,7 @@
 
 //! Main Method for the stub server
 
-use validator_server::Config;
+use validator_server::{Config, Server};
 use mc_common::logger::{create_app_logger, log, o};
 use structopt::StructOpt;
 
@@ -14,6 +14,14 @@ fn main() {
     let config = Config::from_args();
 
     log::info!(logger, "Read Configs: {:?}", config);
+
+    let mut server = Server::new(
+        logger.clone(),
+    );
+    log::info!(logger, "Build server");
+    server.start();
+    log::info!(logger, "Started server");
+
 
     loop {
         std::thread::sleep(std::time::Duration::from_millis(1000));
