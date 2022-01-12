82e9f16aa9fcf524e184e11295d9ce0780c39c45
Eran Rundstein <eran@rundste.in>
2022-01-11 16:56:06 -0800

    basic proto api

--- validator/api/build.rs
+++ validator/api/build.rs
@@ -25,6 +25,12 @@ fn main() {
         .to_owned();
     all_proto_dirs.extend(consensus_api_proto_path.split(':').collect::<Vec<&str>>());
 
+    let fog_report_api_proto_path = env
+        .depvar("MC_FOG_REPORT_API_PROTOS_PATH")
+        .expect("Could not read api's protos path")
+        .to_owned();
+    all_proto_dirs.extend(fog_report_api_proto_path.split(':').collect::<Vec<&str>>());
+
     mc_util_build_grpc::compile_protos_and_generate_mod_rs(
         all_proto_dirs.as_slice(),
         &["validator_api.proto"],
