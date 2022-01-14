f5cf1bf27ac92be28a9c728cf537b2978bfd8400
Christian Oudard <christian.oudard@gmail.com>
2022-01-14 15:02:53 -0800

    Add a performance benchmark for account syncing.
    Add configurable polling delays.
    Increase wait time for client test polling.

--- cli/test/client_tests.py
+++ cli/test/client_tests.py
@@ -20,15 +20,6 @@ def main():
 
     source_wallet = sys.argv[1]
 
-    # Create a test wallet database, and start the server.
-    db_file = tempfile.NamedTemporaryFile(suffix='.db', prefix='test_wallet_', delete=False)
-    cli = CommandLineInterface()
-    cli.config['wallet-db'] = db_file.name
-    cli.stop()
-    time.sleep(0.5)  # Wait for other servers to stop.
-    cli.start(bg=True, unencrypted=True)
-    time.sleep(1.5)  # Wait for the server to start listening.
-
     # Start and end with an empty wallet.
     try:
         check_wallet_empty(c)
@@ -118,7 +109,7 @@ def tests_with_wallet(c, source_wallet):
     source_account_id = source_account['account_id']
 
     # Check its balance and make sure it has txos.
-    balance = c.poll_balance(source_account_id)
+    balance = c.poll_balance(source_account_id, seconds=60)
     assert pmob2mob(balance['unspent_pmob']) >= 1
     txos = c.get_all_txos_for_account(source_account_id)
     assert len(txos) > 0
@@ -293,6 +284,17 @@ def test_gift_codes(c, source_account_id):
     c.remove_account(dest_account_id)
 
     print('PASS')
+
+
+def _start_test_server():
+    # Create a test wallet database, and start the server.
+    db_file = tempfile.NamedTemporaryFile(suffix='.db', prefix='test_wallet_', delete=False)
+    cli = CommandLineInterface()
+    cli.config['wallet-db'] = db_file.name
+    cli.stop()
+    time.sleep(0.5)  # Wait for other servers to stop.
+    cli.start(bg=True, unencrypted=True)
+    time.sleep(1.5)  # Wait for the server to start listening.
 
 
 def check_wallet_empty(c):
---
+++ cli/test/sync_benchmark.py
@@ -0,0 +1,20 @@
+import sys
+import time
+
+from mobilecoin import Client
+from mobilecoin.cli import _load_import
+
+c = Client(verbose=False)
+
+source_wallet = sys.argv[1]
+data = _load_import(source_wallet)
+source_account = c.import_account(**data)
+source_account_id = source_account['account_id']
+
+start = time.monotonic()
+balance = c.poll_balance(source_account_id, seconds=600, poll_delay=0.1)
+end = time.monotonic()
+
+c.remove_account(source_account_id)
+
+print(end - start)
