556aac9d6ccc35e41fef564c166ae8e412a7fb11
david1alvarez <daveyalvarez03@gmail.com>
2022-02-04 15:29:55 -0800

    lint

--- full-service/src/service/gift_code.rs
+++ full-service/src/service/gift_code.rs
@@ -19,7 +19,7 @@ use crate::{
         account::AccountServiceError,
         address::{AddressService, AddressServiceError},
         transaction::{TransactionService, TransactionServiceError},
-        transaction_builder::{DEFAULT_NEW_TX_BLOCK_ATTEMPTS},
+        transaction_builder::DEFAULT_NEW_TX_BLOCK_ATTEMPTS,
         WalletService,
     },
     util::b58::{
@@ -642,7 +642,8 @@ where
         transaction_builder.set_fee(Mob::MINIMUM_FEE)?;
 
         let num_blocks_in_ledger = self.ledger_db.num_blocks()?;
-        transaction_builder.set_tombstone_block(num_blocks_in_ledger + DEFAULT_NEW_TX_BLOCK_ATTEMPTS);
+        transaction_builder
+            .set_tombstone_block(num_blocks_in_ledger + DEFAULT_NEW_TX_BLOCK_ATTEMPTS);
         let tx = transaction_builder.build(&mut rng)?;
 
         let responder_ids = self.peer_manager.responder_ids();
