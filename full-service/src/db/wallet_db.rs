8551849239ac9cf9b70e7b8fd32f91eaf16036dc
Colin Carey <colin.carey1@gmail.com>
2022-02-22 17:04:22 -0800

    Remove bad merge changes

--- full-service/src/db/wallet_db.rs
+++ full-service/src/db/wallet_db.rs
@@ -82,8 +82,7 @@ impl WalletDb {
 
     pub fn set_db_encryption_key_from_env(conn: &SqliteConnection) {
         // Send the encryption key to SQLCipher, if it is not the empty string.
-        // Then check that it worked, or else panic.
-        let encryption_key = env::var("MC_PASSWORD").unwrap_or_default();
+        let encryption_key = env::var("MC_PASSWORD").unwrap_or_else(|_| "".to_string());
         if !encryption_key.is_empty() {
             let result = conn.batch_execute(&format!(
                 "PRAGMA key = {};",
