39c611c70ee859a42ec3a13409909dfa7891a873
Brian <brian.william.corbin@gmail.com>
2022-05-19 12:13:46 -0700

    fixed service/view_only_txo tests

--- full-service/src/service/view_only_txo.rs
+++ full-service/src/service/view_only_txo.rs
@@ -84,93 +84,131 @@ where
     }
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//     use crate::service::view_only_account::ViewOnlyAccountService;
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{
+        db::account::AccountID,
+        service::view_only_account::ViewOnlyAccountService,
+        test_utils::{add_block_to_ledger_db, get_test_ledger, setup_wallet_service, MOB},
+        util::b58::b58_encode_public_address,
+    };
+    use mc_account_keys::{
+        AccountKey, PublicAddress, CHANGE_SUBADDRESS_INDEX, DEFAULT_SUBADDRESS_INDEX,
+    };
+    use mc_common::logger::{test_with_logger, Logger};
+    use mc_crypto_keys::{RistrettoPrivate, RistrettoPublic};
+    use mc_crypto_rand::RngCore;
+    use mc_transaction_core::encrypted_fog_hint::EncryptedFogHint;
+    use mc_util_from_random::FromRandom;
+    use rand::{rngs::StdRng, SeedableRng};
 
-//     use crate::test_utils::{add_block_to_ledger_db, get_test_ledger,
-// setup_wallet_service, MOB};     use mc_account_keys::PublicAddress;
-//     use mc_common::logger::{test_with_logger, Logger};
-//     use mc_crypto_keys::{RistrettoPrivate, RistrettoPublic};
-//     use mc_crypto_rand::RngCore;
-//     use mc_transaction_core::encrypted_fog_hint::EncryptedFogHint;
-//     use mc_util_from_random::FromRandom;
-//     use rand::{rngs::StdRng, SeedableRng};
+    #[test_with_logger]
+    fn test_view_only_txo_service(logger: Logger) {
+        let mut rng: StdRng = SeedableRng::from_seed([20u8; 32]);
+        let known_recipients: Vec<PublicAddress> = Vec::new();
+        let current_block_height = 12; //index 11
+        let mut ledger_db = get_test_ledger(
+            5,
+            &known_recipients,
+            current_block_height as usize,
+            &mut rng,
+        );
+        let service = setup_wallet_service(ledger_db.clone(), logger.clone());
+        let conn = service.wallet_db.get_conn().unwrap();
 
-//     #[test_with_logger]
-//     fn test_view_only_txo_service(logger: Logger) {
-//         let mut rng: StdRng = SeedableRng::from_seed([20u8; 32]);
-//         let known_recipients: Vec<PublicAddress> = Vec::new();
-//         let current_block_height = 12;
-//         let mut ledger_db = get_test_ledger(
-//             5,
-//             &known_recipients,
-//             current_block_height as usize,
-//             &mut rng,
-//         );
-//         let service = setup_wallet_service(ledger_db.clone(),
-// logger.clone());         let conn = service.wallet_db.get_conn().unwrap();
+        let view_private_key = RistrettoPrivate::from_random(&mut rng);
+        let spend_private_key = RistrettoPrivate::from_random(&mut rng);
 
-//         let view_private_key = RistrettoPrivate::from_random(&mut rng);
+        let account_key = AccountKey::new(&spend_private_key, &view_private_key);
+        let account_id = AccountID::from(&account_key);
+        let main_public_address = account_key.default_subaddress();
+        let change_public_address = account_key.change_subaddress();
+        let mut subaddresses: Vec<(String, u64, String, RistrettoPublic)> = Vec::new();
+        subaddresses.push((
+            b58_encode_public_address(&main_public_address).unwrap(),
+            DEFAULT_SUBADDRESS_INDEX,
+            "Main".to_string(),
+            *main_public_address.spend_public_key(),
+        ));
+        subaddresses.push((
+            b58_encode_public_address(&change_public_address).unwrap(),
+            CHANGE_SUBADDRESS_INDEX,
+            "Change".to_string(),
+            *change_public_address.spend_public_key(),
+        ));
 
-//         let account = service
-//             .import_view_only_account(view_private_key.clone(), "coins for
-// cats", None)             .unwrap();
+        let account = service
+            .import_view_only_account(
+                &account_id.to_string(),
+                &view_private_key,
+                DEFAULT_SUBADDRESS_INDEX,
+                CHANGE_SUBADDRESS_INDEX,
+                2,
+                "testing",
+                subaddresses,
+            )
+            .unwrap();
 
-//         let public_address = PublicAddress::new(
-//             &RistrettoPublic::from_random(&mut rng),
-//             &RistrettoPublic::from_random(&mut rng),
-//         );
-//         for _ in 0..2 {
-//             let value = 420;
-//             let tx_private_key = RistrettoPrivate::from_random(&mut rng);
-//             let hint = EncryptedFogHint::fake_onetime_hint(&mut rng);
-//             let fake_tx_out =
-//                 TxOut::new(value as u64, &public_address, &tx_private_key,
-// hint).unwrap();             ViewOnlyTxo::create(fake_tx_out.clone(), value,
-// &account.account_id_hex, &conn)                 .unwrap();
-//         }
+        for _ in 0..2 {
+            let value = 420;
+            let tx_private_key = RistrettoPrivate::from_random(&mut rng);
+            let hint = EncryptedFogHint::fake_onetime_hint(&mut rng);
+            let fake_tx_out =
+                TxOut::new(value as u64, &main_public_address, &tx_private_key, hint).unwrap();
+            ViewOnlyTxo::create(
+                fake_tx_out.clone(),
+                value,
+                Some(DEFAULT_SUBADDRESS_INDEX),
+                Some(11),
+                &account.account_id_hex,
+                &conn,
+            )
+            .unwrap();
+        }
 
-//         let txos = service
-//             .list_view_only_txos(&account.account_id_hex, None, None)
-//             .unwrap();
+        let txos = service
+            .list_view_only_txos(&account.account_id_hex, None, None)
+            .unwrap();
 
-//         for txo in &txos {
-//             assert_eq!(txo.key_image, None);
-//             assert_eq!(txo.spent, false);
-//         }
+        let txo_id_1 = txos[0].txo_id_hex.clone();
+        let txo_id_2 = txos[1].txo_id_hex.clone();
 
-//         let key_image_1 = KeyImage::from(rng.next_u64());
-//         let key_image_2 = KeyImage::from(rng.next_u64());
+        for txo in &txos {
+            assert_eq!(txo.key_image, None);
+            assert_eq!(txo.subaddress_index, Some(DEFAULT_SUBADDRESS_INDEX as i64));
+            assert_eq!(txo.received_block_index, Some(11));
+            assert_eq!(txo.submitted_block_index, None);
+            assert_eq!(txo.pending_tombstone_block_index, None);
+            assert_eq!(txo.spent_block_index, None);
+        }
 
-//         add_block_to_ledger_db(
-//             &mut ledger_db,
-//             &vec![public_address],
-//             42 * MOB,
-//             &vec![key_image_1],
-//             &mut rng,
-//         );
+        let key_image_1 = KeyImage::from(rng.next_u64());
+        let key_image_2 = KeyImage::from(rng.next_u64());
 
-//         let input_vec = [
-//             (mc_util_serial::decode(&txos[0].txo).unwrap(), key_image_1),
-//             (mc_util_serial::decode(&txos[1].txo).unwrap(), key_image_2),
-//         ]
-//         .to_vec();
+        add_block_to_ledger_db(
+            &mut ledger_db,
+            &vec![main_public_address],
+            42 * MOB,
+            &vec![key_image_1],
+            &mut rng,
+        );
 
-//         service.set_view_only_txos_key_images(input_vec).unwrap();
+        let input_vec = [(txo_id_1, key_image_1), (txo_id_2, key_image_2)].to_vec();
 
-//         let txos = service
-//             .list_view_only_txos(&account.account_id_hex, None, None)
-//             .unwrap();
+        service.set_view_only_txos_key_images(input_vec).unwrap();
 
-//         for txo in txos {
-//             assert!(txo.key_image.is_some());
-//             if txo.key_image.unwrap() == mc_util_serial::encode(&key_image_1)
-// {                 assert!(txo.spent);
-//             } else {
-//                 assert_eq!(txo.spent, false);
-//             }
-//         }
-//     }
-// }
+        let txos = service
+            .list_view_only_txos(&account.account_id_hex, None, None)
+            .unwrap();
+
+        for txo in txos {
+            assert!(txo.key_image.is_some());
+            if txo.key_image.unwrap() == mc_util_serial::encode(&key_image_1) {
+                assert_eq!(txo.spent_block_index, Some(12));
+            } else {
+                assert_eq!(txo.spent_block_index, None);
+            }
+        }
+    }
+}
