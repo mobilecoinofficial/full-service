73d3150f558992f21ffb6bb55ef8ebc20990b943
Brian <brian.william.corbin@gmail.com>
2022-06-08 10:56:14 -0700

    add test for min and max block transaction logs

--- full-service/src/service/transaction_log.rs
+++ full-service/src/service/transaction_log.rs
@@ -130,3 +130,121 @@ where
         Ok(res)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::{
+        db::account::AccountID,
+        service::{account::AccountService, transaction_log::TransactionLogService},
+        test_utils::{
+            add_block_to_ledger_db, get_test_ledger, manually_sync_account, setup_wallet_service,
+            MOB,
+        },
+    };
+    use mc_account_keys::{AccountKey, PublicAddress};
+    use mc_common::logger::{test_with_logger, Logger};
+    use mc_crypto_rand::rand_core::RngCore;
+    use mc_transaction_core::ring_signature::KeyImage;
+    use rand::{rngs::StdRng, SeedableRng};
+
+    #[test_with_logger]
+    fn test_list_transaction_logs_for_account_with_min_and_max_block_index(logger: Logger) {
+        let mut rng: StdRng = SeedableRng::from_seed([20u8; 32]);
+
+        let known_recipients: Vec<PublicAddress> = Vec::new();
+        let mut ledger_db = get_test_ledger(5, &known_recipients, 12, &mut rng);
+
+        let service = setup_wallet_service(ledger_db.clone(), logger.clone());
+
+        // Create our main account for the wallet
+        let alice = service
+            .create_account(
+                Some("Alice's Main Account".to_string()),
+                "".to_string(),
+                "".to_string(),
+                "".to_string(),
+            )
+            .unwrap();
+
+        let alice_account_key: AccountKey = mc_util_serial::decode(&alice.account_key).unwrap();
+        let alice_account_id = AccountID::from(&alice_account_key);
+        let alice_public_address = alice_account_key.subaddress(alice.main_subaddress_index as u64);
+
+        let tx_logs = service
+            .list_transaction_logs(&alice_account_id, None, None, None, None)
+            .unwrap();
+
+        assert_eq!(0, tx_logs.len());
+
+        // block_index 12
+        add_block_to_ledger_db(
+            &mut ledger_db,
+            &vec![alice_public_address.clone()],
+            100 * MOB,
+            &vec![KeyImage::from(rng.next_u64())],
+            &mut rng,
+        );
+
+        // block_index 13
+        add_block_to_ledger_db(
+            &mut ledger_db,
+            &vec![alice_public_address.clone()],
+            100 * MOB,
+            &vec![KeyImage::from(rng.next_u64())],
+            &mut rng,
+        );
+
+        // block_index 14
+        add_block_to_ledger_db(
+            &mut ledger_db,
+            &vec![alice_public_address.clone()],
+            100 * MOB,
+            &vec![KeyImage::from(rng.next_u64())],
+            &mut rng,
+        );
+
+        // block_index 15
+        add_block_to_ledger_db(
+            &mut ledger_db,
+            &vec![alice_public_address.clone()],
+            100 * MOB,
+            &vec![KeyImage::from(rng.next_u64())],
+            &mut rng,
+        );
+
+        // block_index 16
+        add_block_to_ledger_db(
+            &mut ledger_db,
+            &vec![alice_public_address.clone()],
+            100 * MOB,
+            &vec![KeyImage::from(rng.next_u64())],
+            &mut rng,
+        );
+
+        manually_sync_account(&ledger_db, &service.wallet_db, &alice_account_id, &logger);
+
+        let tx_logs = service
+            .list_transaction_logs(&alice_account_id, None, None, None, None)
+            .unwrap();
+
+        assert_eq!(5, tx_logs.len());
+
+        let tx_logs = service
+            .list_transaction_logs(&alice_account_id, None, None, Some(15), None)
+            .unwrap();
+
+        assert_eq!(2, tx_logs.len());
+
+        let tx_logs = service
+            .list_transaction_logs(&alice_account_id, None, None, None, Some(13))
+            .unwrap();
+
+        assert_eq!(2, tx_logs.len());
+
+        let tx_logs = service
+            .list_transaction_logs(&alice_account_id, None, None, Some(13), Some(15))
+            .unwrap();
+
+        assert_eq!(3, tx_logs.len());
+    }
+}
