d2fa20424c95c8f6024da2e626a63473590e0bd8
David Alvarez <daveyalvarez03@gmail.com>
2022-06-16 18:15:52 -0700

    build fs

--- tools/build_full_service.py
+++ tools/build_full_service.py
@@ -15,7 +15,7 @@ cmd=' '.join([
   'MC_SEED=a4aa76e4a5ca70c8447dd544a63f180b5a6fe0aff96495802506354c10f2886e',
   'SGX_MODE=SW',
   'IAS_MODE=DEV',
-  f'CONSENSUS_ENCLAVE_CSS={MOBILECOIN_DIR}/consensus-enclave.css',
+  f'CONSENSUS_ENCLAVE_CSS={MOBILECOIN_DIR}/target/release/consensus-enclave.css',
   'cargo build --release -p mc-full-service'
 ])
 # build = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, universal_newlines=True)
