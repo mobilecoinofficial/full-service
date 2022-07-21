d7010924101899831aec5a47e9c75a0d82afa92d
Brian <brian.william.corbin@gmail.com>
2022-07-20 19:17:53 -0700

    updating build/submit tests with non-Mob tokens

--- full-service/src/json_rpc/e2e_tests/transaction/build_submit/build_then_submit.rs
+++ full-service/src/json_rpc/e2e_tests/transaction/build_submit/build_then_submit.rs
@@ -11,14 +11,22 @@ mod e2e_transaction {
             tx_proposal::TxProposal as TxProposalJSON,
         },
         service::models::tx_proposal::TxProposal,
-        test_utils::{add_block_to_ledger_db, add_block_with_tx_proposal, manually_sync_account},
+        test_utils::{
+            add_block_to_ledger_db, add_block_with_tx_outs, add_block_with_tx_proposal,
+            manually_sync_account,
+        },
         util::b58::b58_decode_public_address,
     };
 
+    use mc_blockchain_types::BlockVersion;
     use mc_common::logger::{test_with_logger, Logger};
+    use mc_crypto_keys::RistrettoPrivate;
     use mc_crypto_rand::rand_core::RngCore;
     use mc_ledger_db::Ledger;
-    use mc_transaction_core::{ring_signature::KeyImage, tokens::Mob, Token};
+    use mc_transaction_core::{
+        ring_signature::KeyImage, tokens::Mob, tx::TxOut, Amount, Token, TokenId,
+    };
+    use mc_util_from_random::FromRandom;
     use rand::{rngs::StdRng, SeedableRng};
 
     use std::convert::TryFrom;
@@ -97,7 +105,7 @@ mod e2e_transaction {
         // Add a block with significantly more MOB
         add_block_to_ledger_db(
             &mut ledger_db,
-            &vec![public_address],
+            &vec![public_address.clone()],
             100000000000000, // 100.0 MOB
             &vec![KeyImage::from(rng.next_u64())],
             &mut rng,
@@ -348,5 +356,197 @@ mod e2e_transaction {
             .as_object()
             .unwrap();
         assert_eq!(transaction_log_map.len(), 1);
+
+        let txo = TxOut::new(
+            BlockVersion::MAX,
+            Amount::new(1000000000000, TokenId::from(1)),
+            &public_address,
+            &RistrettoPrivate::from_random(&mut rng),
+            Default::default(),
+        )
+        .unwrap();
+
+        add_block_with_tx_outs(
+            &mut ledger_db,
+            &[txo],
+            &vec![KeyImage::from(rng.next_u64())],
+            &mut rng,
+        );
+
+        manually_sync_account(
+            &ledger_db,
+            &db_ctx.get_db_instance(logger.clone()),
+            &AccountID(account_id.to_string()),
+            &logger,
+        );
+
+        // Create a tx proposal to ourselves, but this should fail because we cannot yet
+        // do mixed token transactions
+        let body = json!({
+            "jsonrpc": "2.0",
+            "id": 1,
+            "method": "build_transaction",
+            "params": {
+                "account_id": account_id,
+                "recipient_public_address": b58_public_address,
+                "amount": { "value": "500000000000", "token_id": "1" },
+                "fee_token_id": "0",
+            }
+        });
+        let res = dispatch(&client, body, &logger);
+        let err = res.get("error");
+        assert!(err.is_some());
+
+        let body = json!({
+            "jsonrpc": "2.0",
+            "id": 1,
+            "method": "build_transaction",
+            "params": {
+                "account_id": account_id,
+                "recipient_public_address": b58_public_address,
+                "amount": { "value": "500000000000", "token_id": "1" }
+            }
+        });
+        let res = dispatch(&client, body, &logger);
+        let result = res.get("result").unwrap();
+        let tx_proposal = result.get("tx_proposal").unwrap();
+
+        let fee_token_id = tx_proposal.get("fee_token_id").unwrap();
+        assert_eq!(fee_token_id, "1");
+
+        let inputs = tx_proposal.get("input_txos").unwrap().as_array().unwrap();
+        assert_eq!(inputs.len(), 1);
+
+        // One destination
+        let payload_txos = tx_proposal.get("payload_txos").unwrap().as_array().unwrap();
+        assert_eq!(payload_txos.len(), 1);
+
+        let change_txos = tx_proposal.get("change_txos").unwrap().as_array().unwrap();
+        assert_eq!(change_txos.len(), 1);
+
+        // Tombstone block = ledger height (12 to start + 4 new blocks + 10 default
+        // tombstone)
+        let tombstone_block_index = tx_proposal.get("tombstone_block_index").unwrap();
+        assert_eq!(tombstone_block_index, "26");
+
+        // Get current balance
+        assert_eq!(ledger_db.num_blocks().unwrap(), 16);
+        let body = json!({
+            "jsonrpc": "2.0",
+            "id": 1,
+            "method": "get_balance_for_account",
+            "params": {
+                "account_id": account_id,
+            }
+        });
+        let res = dispatch(&client, body, &logger);
+        let result = res.get("result").unwrap();
+        let balance_per_token = result.get("balance_per_token").unwrap();
+        let balance_1 = balance_per_token.get("1").unwrap();
+        let unspent = balance_1["unspent"].as_str().unwrap();
+        assert_eq!(unspent, "1000000000000");
+
+        // Submit the tx_proposal
+        let body = json!({
+            "jsonrpc": "2.0",
+            "id": 1,
+            "method": "submit_transaction",
+            "params": {
+                "tx_proposal": tx_proposal,
+                "account_id": account_id,
+            }
+        });
+        let res = dispatch(&client, body, &logger);
+        let _result = res.get("result").unwrap();
+        let json_tx_proposal: TxProposalJSON = serde_json::from_value(tx_proposal.clone()).unwrap();
+        let payments_tx_proposal = TxProposal::try_from(&json_tx_proposal).unwrap();
+
+        // Get balance after submission
+        let body = json!({
+            "jsonrpc": "2.0",
+            "id": 1,
+            "method": "get_balance_for_account",
+            "params": {
+                "account_id": account_id,
+            }
+        });
+        let res = dispatch(&client, body, &logger);
+        let result = res.get("result").unwrap();
+        let balance_per_token = result.get("balance_per_token").unwrap();
+
+        // Balance of MOB should be unchanged
+        let balance_mob = balance_per_token.get(Mob::ID.to_string()).unwrap();
+        let unspent = balance_mob["unspent"].as_str().unwrap();
+        let pending = balance_mob["pending"].as_str().unwrap();
+        let spent = balance_mob["spent"].as_str().unwrap();
+        let secreted = balance_mob["secreted"].as_str().unwrap();
+        let orphaned = balance_mob["orphaned"].as_str().unwrap();
+        assert_eq!(unspent, &(100000000000100 - Mob::MINIMUM_FEE).to_string());
+        assert_eq!(pending, "0");
+        assert_eq!(spent, "100000000000100");
+        assert_eq!(secreted, "0");
+        assert_eq!(orphaned, "0");
+
+        // There should be a pending balance for this token now
+        let balance_1 = balance_per_token.get("1").unwrap();
+        let unspent = balance_1["unspent"].as_str().unwrap();
+        let pending = balance_1["pending"].as_str().unwrap();
+        let spent = balance_1["spent"].as_str().unwrap();
+        let secreted = balance_1["secreted"].as_str().unwrap();
+        let orphaned = balance_1["orphaned"].as_str().unwrap();
+        assert_eq!(unspent, "0");
+        assert_eq!(pending, "1000000000000");
+        assert_eq!(spent, "0");
+        assert_eq!(secreted, "0");
+        assert_eq!(orphaned, "0");
+
+        // The MockBlockchainConnection does not write to the ledger_db
+        add_block_with_tx_proposal(&mut ledger_db, payments_tx_proposal, &mut rng);
+
+        manually_sync_account(
+            &ledger_db,
+            &db_ctx.get_db_instance(logger.clone()),
+            &AccountID(account_id.to_string()),
+            &logger,
+        );
+        assert_eq!(ledger_db.num_blocks().unwrap(), 17);
+
+        // Get balance after submission
+        let body = json!({
+            "jsonrpc": "2.0",
+            "id": 1,
+            "method": "get_balance_for_account",
+            "params": {
+                "account_id": account_id,
+            }
+        });
+        let res = dispatch(&client, body, &logger);
+        let result = res.get("result").unwrap();
+        let balance_per_token = result.get("balance_per_token").unwrap();
+
+        // Balance of MOB should be unchanged
+        let balance_mob = balance_per_token.get(Mob::ID.to_string()).unwrap();
+        let unspent = balance_mob["unspent"].as_str().unwrap();
+        let pending = balance_mob["pending"].as_str().unwrap();
+        let spent = balance_mob["spent"].as_str().unwrap();
+        let secreted = balance_mob["secreted"].as_str().unwrap();
+        let orphaned = balance_mob["orphaned"].as_str().unwrap();
+        assert_eq!(unspent, &(100000000000100 - Mob::MINIMUM_FEE).to_string());
+        assert_eq!(pending, "0");
+        assert_eq!(spent, "100000000000100");
+        assert_eq!(secreted, "0");
+        assert_eq!(orphaned, "0");
+
+        let balance_1 = balance_per_token.get("1").unwrap();
+        let unspent = balance_1["unspent"].as_str().unwrap();
+        let pending = balance_1["pending"].as_str().unwrap();
+        let spent = balance_1["spent"].as_str().unwrap();
+        let secreted = balance_1["secreted"].as_str().unwrap();
+        let orphaned = balance_1["orphaned"].as_str().unwrap();
+        assert_eq!(unspent, "999999998976".to_string());
+        assert_eq!(pending, "0");
+        assert_eq!(spent, "1000000000000");
+        assert_eq!(secreted, "0");
+        assert_eq!(orphaned, "0");
     }
 }
