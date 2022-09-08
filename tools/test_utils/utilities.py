1aa23cfe834cbde8edbda126718e64eeae4e26c6
Pouneh Aghababazadeh <paghababazadeh@gmail.com>
2022-09-08 09:30:28 -0700

    Move the argument parsing to a test utility

---
+++ tools/test_utils/utilities.py
@@ -0,0 +1,8 @@
+import argparse
+
+def parse_cmd_line_args():
+    # pull args from command line
+    parser = argparse.ArgumentParser(description='Local network tester')
+    parser.add_argument('--network-type', help='Type of network to create', required=True)
+    parser.add_argument('--block-version', help='Set the block version argument', type=int)
+    return parser.parse_args()
