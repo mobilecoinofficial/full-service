81a5c175619afeceb2b89f52ea104d2093bc61a4
Pouneh Aghababazadeh <paghababazadeh@gmail.com>
2022-09-08 09:31:48 -0700

    Rename function

--- tools/test_utils/utilities.py
+++ tools/test_utils/utilities.py
@@ -1,6 +1,6 @@
 import argparse
 
-def parse_cmd_line_args():
+def parse_network_type_cmd_line_args():
     # pull args from command line
     parser = argparse.ArgumentParser(description='Local network tester')
     parser.add_argument('--network-type', help='Type of network to create', required=True)
