60919a79504746b762fe2d92da4ee17733e75122
cxloe <cxl0e@protonmail.com>
2022-09-21 18:12:20 -0400

    black, comment

--- tools/test_utils/fullservice.py
+++ tools/test_utils/fullservice.py
@@ -1,7 +1,9 @@
 # Copyright (c) 2022 MobileCoin, Inc.
 
-# TODO: This should actually be more generic so that the python CLI 
+# TODO: This should actually be more generic so that the python CLI
 #   can also use it as a library (or maybe tests will use the CLI's library)
+
+# todo: organize imports
 import asyncio
 from unittest import result
 from urllib import request
@@ -14,7 +16,7 @@ import shutil
 import subprocess
 import time
 import logging
-import ssl 
+import ssl
 import base64
 
 from typing import Any, Optional
@@ -38,6 +40,7 @@ else:
     ssl_context.verify_mode = ssl.CERT_REQUIRED
     ssl_context.load_cert_chain(certfile="client.full.pem")
 
+
 class FullService:
 
     default_url = ()
@@ -55,15 +58,14 @@ class FullService:
             ).removesuffix("/wallet") + "/wallet"
         logging.info("full-service url: %s", url)
         self.url = url
-        
-        
+
     def __enter__(self):
         self.remove_wallet_and_ledger = True
         self.start()
         return self
-    
-    #this is test specific, to be moved 
-    def __exit__(self,exc_type, exc_val, exc_tb):
+
+    # this is test specific, to be moved
+    def __exit__(self, exc_type, exc_val, exc_tb):
         self.stop()
         if self.remove_wallet_and_ledger:
             try:
@@ -71,7 +73,7 @@ class FullService:
                 shutil.rmtree(self.wallet_path)
                 shutil.rmtree(self.ledger_path)
             except Exception as e:
-                print(e) 
+                print(e)
 
     def stop(self):
         try:
@@ -91,7 +93,7 @@ class FullService:
     async def request(self, request_data):
         self.request_count += 1
         request_data = {"jsonrpc": "2.0", "id": self.request_count, **request_data}
-        print(f'request data: {request_data}')
+        print(f"request data: {request_data}")
         async with aiohttp.TCPConnector(ssl=ssl_context) as conn:
             async with aiohttp.ClientSession(connector=conn) as sess:
                 # this can hang (forever?) if there's no full-service at that url
@@ -104,79 +106,72 @@ class FullService:
                     return await resp.json()
 
     def import_account(self, mnemonic) -> bool:
-        print(f'importing full service account {mnemonic}')
+        print(f"importing full service account {mnemonic}")
         params = {
-            'mnemonic': mnemonic,
-            'key_derivation_version': '2',
+            "mnemonic": mnemonic,
+            "key_derivation_version": "2",
         }
-        r = self.request({
-            "method": "import_account",
-            "params": params
-        })
+        r = self.request({"method": "import_account", "params": params})
 
-        if 'error' in r:
+        if "error" in r:
             # If we failed due to a unique constraint, it means the account already exists
-            return 'Diesel Error: UNIQUE constraint failed' in r['error']['data']['details']
+            return (
+                "Diesel Error: UNIQUE constraint failed"
+                in r["error"]["data"]["details"]
+            )
         return True
 
     # check if full service is synced within margin
     def sync_status(self, eps=5) -> bool:
         # ping network
         try:
-            r = self.request({
-                "method": "get_network_status"
-            })
+            r = self.request({"method": "get_network_status"})
         except ConnectionError as e:
             print(e)
             return False
 
         # network offline
-        if int(r['network_status']['network_block_height']) == 0:
+        if int(r["network_status"]["network_block_height"]) == 0:
             return False
 
         # network online
-        network_block_height = int(r['network_status']['network_block_height'])
-        local_block_height = int(r['network_status']['local_block_height'])
+        network_block_height = int(r["network_status"]["network_block_height"])
+        local_block_height = int(r["network_status"]["local_block_height"])
 
         # network is acceptably synced
-        return (network_block_height - local_block_height < eps)
+        return network_block_height - local_block_height < eps
 
     # retrieve wallet status
     def get_wallet_status(self):
-        r = self.request({
-            "method": "get_wallet_status"
-        })
-        return r['wallet_status']
+        r = self.request({"method": "get_wallet_status"})
+        return r["wallet_status"]
 
     # retrieve all accounts full service is aware of
     def get_all_accounts(self) -> Tuple[list, dict]:
         r = self.request({"method": "get_all_accounts"})
         print(r)
-        return (r['account_ids'], r['account_map'])
+        return (r["account_ids"], r["account_map"])
 
     # retrieve information about account
     def get_account_status(self, account_id: str):
-        params = {
-            "account_id": account_id
-        }
-        r = self.request({
-            "method": "get_account_status",
-            "params": params
-        })
+        params = {"account_id": account_id}
+        r = self.request({"method": "get_account_status", "params": params})
         return r
 
     # build and submit a transaction from `account_id` to `to_address` for `amount` of pmob
     def send_transaction(self, account_id, to_address, amount):
         params = {
             "account_id": account_id,
-            "addresses_and_values": [(to_address, amount)]
+            "addresses_and_values": [(to_address, amount)],
         }
-        r = self.request({
-            "method": "build_and_submit_transaction",
-            "params": params,
-        })
+        r = self.request(
+            {
+                "method": "build_and_submit_transaction",
+                "params": params,
+            }
+        )
         print(r)
-        return r['transaction_log']
+        return r["transaction_log"]
 
     def sync_full_service_to_network(self, mc_network):
         network_synced = False
@@ -186,8 +181,8 @@ class FullService:
             count += 1
             network_synced = self.sync_status()
             if count % 10 == 0:
-                print(f'attempt: {count}/{attempt_limit}')
+                print(f"attempt: {count}/{attempt_limit}")
             time.sleep(1)
         if count >= attempt_limit:
-            raise Exception(f'Full service sync failed after {attempt_limit} attempts')
-        print('Full service synced')
+            raise Exception(f"Full service sync failed after {attempt_limit} attempts")
+        print("Full service synced")
