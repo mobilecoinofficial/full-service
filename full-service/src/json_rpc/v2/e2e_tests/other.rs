c9aafc48bd1222a847ea6517cf17441bb396d2ac
Brian <brian.william.corbin@gmail.com>
2022-09-22 14:57:24 -0700

    added tests

--- full-service/src/json_rpc/v2/e2e_tests/other.rs
+++ full-service/src/json_rpc/v2/e2e_tests/other.rs
@@ -4,15 +4,18 @@
 
 #[cfg(test)]
 mod e2e_misc {
-    use crate::json_rpc::v2::api::test_utils::{
-        dispatch, dispatch_with_header, dispatch_with_header_expect_error, setup,
-        setup_with_api_key,
+    use crate::{
+        json_rpc::v2::api::test_utils::{
+            dispatch, dispatch_with_header, dispatch_with_header_expect_error, setup,
+            setup_with_api_key, wait_for_sync,
+        },
+        test_utils::{
+            add_block_with_tx_outs, create_test_received_txo, random_account_with_seed_values, MOB,
+        },
     };
-
     use mc_common::logger::{test_with_logger, Logger};
-
-    use mc_transaction_core::{tokens::Mob, BlockVersion, Token};
-
+    use mc_crypto_rand::RngCore;
+    use mc_transaction_core::{ring_signature::KeyImage, tokens::Mob, Amount, BlockVersion, Token};
     use rand::{rngs::StdRng, SeedableRng};
     use rocket::http::{Header, Status};
 
@@ -117,6 +120,70 @@ mod e2e_misc {
         assert_eq!(
             fees.get(&Mob::ID.to_string()).unwrap().as_str().unwrap(),
             &Mob::MINIMUM_FEE.to_string()
+        );
+    }
+
+    #[test_with_logger]
+    fn test_validate_tx_out(logger: Logger) {
+        let mut rng: StdRng = SeedableRng::from_seed([20u8; 32]);
+        let (client, mut ledger_db, db_ctx, network_state) = setup(&mut rng, logger.clone());
+        let wallet_db = db_ctx.get_db_instance(logger.clone());
+        let account_key = random_account_with_seed_values(
+            &wallet_db,
+            &mut ledger_db,
+            &vec![70 * MOB],
+            &mut rng,
+            &logger,
+        );
+
+        let (_, tx_out, _) = create_test_received_txo(
+            &account_key,
+            0,
+            Amount::new(70, Mob::ID),
+            13,
+            &mut rng,
+            &wallet_db,
+        );
+
+        add_block_with_tx_outs(
+            &mut ledger_db,
+            &[tx_out.clone()],
+            &[KeyImage::from(rng.next_u64())],
+            &mut rng,
+        );
+        wait_for_sync(&client, &ledger_db, &network_state, &logger);
+
+        // A valid public key on the ledger
+        let public_key = hex::encode(tx_out.public_key.as_bytes());
+
+        let body = json!({
+            "jsonrpc": "2.0",
+            "id": 1,
+            "method": "validate_tx_out",
+            "params": {
+                "public_key": public_key
+            }
+        });
+        let res = dispatch(&client, body, &logger);
+        let result = res.get("result").unwrap();
+        assert_eq!(result.get("block_index").unwrap(), "13");
+
+        // An invalid public key on the ledger
+        let target_key = hex::encode(tx_out.target_key.as_bytes());
+
+        let body = json!({
+            "jsonrpc": "2.0",
+            "id": 1,
+            "method": "validate_tx_out",
+            "params": {
+                "public_key": target_key
+            }
+        });
+        let res = dispatch(&client, body, &logger);
+        let error = res.get("error").unwrap();
+        assert_eq!(
+            error.get("data").unwrap().get("server_error").unwrap(),
+            "LedgerDB(NotFound)"
         );
     }
 }
