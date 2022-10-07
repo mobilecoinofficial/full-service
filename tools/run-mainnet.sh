036537ee9ae5563c81384aeff0a8574c92d351b5
Pouneh Aghababazadeh <paghababazadeh@gmail.com>
2022-10-06 21:45:19 -0700

    Change namespace and build FS when running mainnet.sh

--- tools/run-mainnet.sh
+++ tools/run-mainnet.sh
@@ -1,22 +1,32 @@
-NAMESPACE=main
+NAMESPACE=prod
 
 WORK_DIR="$HOME/.mobilecoin/${NAMESPACE}"
 WALLET_DB_DIR="${WORK_DIR}/wallet-db"
 LEDGER_DB_DIR="${WORK_DIR}/ledger-db"
 mkdir -p ${WORK_DIR}
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-(cd ${WORK_DIR} && CONSENSUS_SIGSTRUCT_URI=$(curl -s https://enclave-distribution.${NAMESPACE}.mobilecoin.com/production.json | grep consensus-enclave.css | awk '{print $2}' | tr -d \" | tr -d ,)
-curl -O https://enclave-distribution.${NAMESPACE}.mobilecoin.com/${CONSENSUS_SIGSTRUCT_URI})
+if [ $# -eq 0 ]; then
+FS_DIR=$WORK_DIR
+else
+FS_DIR="$SCRIPT_DIR/../target/release"
+fi
+
+INGEST_ENCLAVE_CSS="$WORK_DIR/ingest-enclave.css"
 
 (cd ${WORK_DIR} && INGEST_SIGSTRUCT_URI=$(curl -s https://enclave-distribution.${NAMESPACE}.mobilecoin.com/production.json | grep ingest-enclave.css | awk '{print $2}' | tr -d \" | tr -d ,)
 curl -O https://enclave-distribution.${NAMESPACE}.mobilecoin.com/${INGEST_SIGSTRUCT_URI})
 
+
+($SCRIPT_DIR/build-fs.sh main)
+
 mkdir -p ${WALLET_DB_DIR}
-${WORK_DIR}/full-service \
+${FS_DIR}/full-service \
     --wallet-db ${WALLET_DB_DIR}/wallet.db \
     --ledger-db ${LEDGER_DB_DIR} \
     --peer mc://node1.prod.mobilecoinww.com/ \
     --peer mc://node2.prod.mobilecoinww.com/ \
     --tx-source-url https://ledger.mobilecoinww.com/node1.prod.mobilecoinww.com/ \
     --tx-source-url https://ledger.mobilecoinww.com/node2.prod.mobilecoinww.com/ \
-    --fog-ingest-enclave-css $(pwd)/ingest-enclave.css
+    --fog-ingest-enclave-css $INGEST_ENCLAVE_CSS \
+    --chain-id main
