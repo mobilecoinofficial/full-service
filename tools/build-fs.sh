da19343cbfb2e3424a530a7e5b034d9587a6e7d0
Pouneh Aghababazadeh <paghababazadeh@gmail.com>
2022-10-07 13:55:58 -0700

    Use exports instead of variable assignment and then reuse/reassignment later

--- tools/build-fs.sh
+++ tools/build-fs.sh
@@ -16,30 +16,25 @@ exit 1
 elif [ $NET == "test" ]; then
 echo "Building Full Service with testnet settings"
 NAMESPACE="test"
-SM=SW
-IM=DEV
+export SGX_MODE=SW
+export IAS_MODE=DEV
 elif [ $NET == "main" ]; then
 NAMESPACE="prod"
-SM=HW
-IM=PROD
+export SGX_MODE=HW
+export IAS_MODE=PROD
 else
 echo "Network specified is not valid. Try 'test' or 'main'. Default is test"
 exit 1
 fi
 
 WORK_DIR="$HOME/.mobilecoin/${NET}"
-WALLET_DB_DIR="${WORK_DIR}/wallet-db"
-LEDGER_DB_DIR="${WORK_DIR}/ledger-db"
 mkdir -p ${WORK_DIR}
 
-CONSENSUS_ENCLAVE_CSS="$WORK_DIR/consensus-enclave.css"
+export CONSENSUS_ENCLAVE_CSS="$WORK_DIR/consensus-enclave.css"
 
 (cd ${WORK_DIR} && CONSENSUS_SIGSTRUCT_URI=$(curl -s https://enclave-distribution.${NAMESPACE}.mobilecoin.com/production.json | grep consensus-enclave.css | awk '{print $2}' | tr -d \" | tr -d ,)
 curl -O https://enclave-distribution.${NAMESPACE}.mobilecoin.com/${CONSENSUS_SIGSTRUCT_URI})
 
 echo "building full service..."
-SGX_MODE=$SM \
-IAS_MODE=$IM \
-CONSENSUS_ENCLAVE_CSS=$CONSENSUS_ENCLAVE_CSS \
 cargo build --release -p mc-full-service
 
