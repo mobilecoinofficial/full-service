f02efb03cfbf620e5349caaaeef3aabf1baa9e2b
Pouneh Aghababazadeh <paghababazadeh@gmail.com>
2022-10-07 16:48:32 -0700

    Remove unecessary parenthesis

--- tools/run-mainnet.sh
+++ tools/run-mainnet.sh
@@ -8,19 +8,20 @@ LEDGER_DB_DIR="${WORK_DIR}/ledger-db"
 mkdir -p ${WORK_DIR}
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-if [ $# -eq 0 ]; then
-FS_DIR=$WORK_DIR
-else
-FS_DIR="$SCRIPT_DIR/../target/release"
-fi
-
 INGEST_ENCLAVE_CSS="$WORK_DIR/ingest-enclave.css"
 
 (cd ${WORK_DIR} && INGEST_SIGSTRUCT_URI=$(curl -s https://enclave-distribution.${NAMESPACE}.mobilecoin.com/production.json | grep ingest-enclave.css | awk '{print $2}' | tr -d \" | tr -d ,)
 curl -O https://enclave-distribution.${NAMESPACE}.mobilecoin.com/${INGEST_SIGSTRUCT_URI})
 
+$SCRIPT_DIR/build-fs.sh main
 
-($SCRIPT_DIR/build-fs.sh main)
+# Default is to run whatver binary is sitting in the directory under mobilecoin named $NAMESPACE
+# However, the build script by default drops the binary in the release folder.
+if [ $# -eq 0 ]; then
+FS_DIR=$WORK_DIR
+else
+FS_DIR="$SCRIPT_DIR/../target/release"
+fi
 
 mkdir -p ${WALLET_DB_DIR}
 ${FS_DIR}/full-service \
