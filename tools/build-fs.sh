45ad0e399dc49f2494d1b18d9e40506f8412c9b0
Pouneh Aghababazadeh <paghababazadeh@gmail.com>
2022-10-10 11:52:12 -0700

    OOPS: syntax error

--- tools/build-fs.sh
+++ tools/build-fs.sh
@@ -33,7 +33,7 @@ if [ $NET -eq "test" ] || [ $NET -eq "main" ]; then
     export CONSENSUS_ENCLAVE_CSS="$WORK_DIR/consensus-enclave.css"
     (cd ${WORK_DIR} && CONSENSUS_SIGSTRUCT_URI=$(curl -s https://enclave-distribution.${NAMESPACE}.mobilecoin.com/production.json | grep consensus-enclave.css | awk '{print $2}' | tr -d \" | tr -d ,)
         curl -O https://enclave-distribution.${NAMESPACE}.mobilecoin.com/${CONSENSUS_SIGSTRUCT_URI})
-else; 
+else
     echo "Using current environment's SGX_MODE, IAS_MODE, CONSENSUS_ENCLAVE_CSS "
 fi
 
