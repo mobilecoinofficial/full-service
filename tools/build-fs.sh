50e82362420c6c0a11fe9ac036ac0299995ee6cc
Pouneh Aghababazadeh <paghababazadeh@gmail.com>
2022-10-10 11:51:21 -0700

    Invert conditions so we check for test/main first, then for all others do the default operation

--- tools/build-fs.sh
+++ tools/build-fs.sh
@@ -16,9 +16,7 @@ if [ $# -gt 0 ]; then
 NET=$1
 fi
 
-if [ -z ${NET} ] || [ $NET -ne "test" ] || [ $NET -ne "main" ]; then
-    echo "Using current environment's SGX_MODE, IAS_MODE, CONSENSUS_ENCLAVE_CSS "
-elif [ $NET -eq "test" ] || [ $NET -eq "main" ]; then
+if [ $NET -eq "test" ] || [ $NET -eq "main" ]; then
     if [ $NET == "test" ]; then
     echo "Building Full Service with testnet settings"
     NAMESPACE="test"
@@ -35,8 +33,8 @@ elif [ $NET -eq "test" ] || [ $NET -eq "main" ]; then
     export CONSENSUS_ENCLAVE_CSS="$WORK_DIR/consensus-enclave.css"
     (cd ${WORK_DIR} && CONSENSUS_SIGSTRUCT_URI=$(curl -s https://enclave-distribution.${NAMESPACE}.mobilecoin.com/production.json | grep consensus-enclave.css | awk '{print $2}' | tr -d \" | tr -d ,)
         curl -O https://enclave-distribution.${NAMESPACE}.mobilecoin.com/${CONSENSUS_SIGSTRUCT_URI})
-else
-    echo "Not sure what happens here..."
+else; 
+    echo "Using current environment's SGX_MODE, IAS_MODE, CONSENSUS_ENCLAVE_CSS "
 fi
 
 echo "building full service..."
