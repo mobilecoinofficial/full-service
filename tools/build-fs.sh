510412bfb5b069340e8bef57296947087999284d
p <paghababazadeh@gmail.com>
2022-10-10 15:49:48 -0700

    Update tools/build-fs.sh
    Co-authored-by: Eran Rundstein <eran@rundste.in>

--- tools/build-fs.sh
+++ tools/build-fs.sh
@@ -30,7 +30,7 @@ if [ $NET == "test" ] || [ $NET == "main" ]; then
     (cd ${WORK_DIR} && CONSENSUS_SIGSTRUCT_URI=$(curl -s https://enclave-distribution.${NAMESPACE}.mobilecoin.com/production.json | grep consensus-enclave.css | awk '{print $2}' | tr -d \" | tr -d ,)
         curl -O https://enclave-distribution.${NAMESPACE}.mobilecoin.com/${CONSENSUS_SIGSTRUCT_URI})
 else
-    echo "Using current environment's SGX_MODE, IAS_MODE, CONSENSUS_ENCLAVE_CSS "
+    echo "Using current environment's SGX_MODE, IAS_MODE, CONSENSUS_ENCLAVE_CSS"
 fi
 
 exit 0
