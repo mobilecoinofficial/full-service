d81ac2d3dd1a664b0f3663babaa0516f528803d4
Pouneh Aghababazadeh <paghababazadeh@gmail.com>
2022-10-10 13:14:11 -0700

    Add a no-build flag

--- tools/run-mainnet.sh
+++ tools/run-mainnet.sh
@@ -8,19 +8,23 @@ WALLET_DB_DIR="${WORK_DIR}/wallet-db"
 LEDGER_DB_DIR="${WORK_DIR}/ledger-db"
 
 
-mkdir -p ${WORK_DIR}
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+# Default is to run whatver binary is sitting in the directory under mobilecoin named $NAMESPACE
+# However, the build script by default drops the binary in the release folder.
+if [ $# -eq 0 ] || [ $1 != '--no-build']; then
 
-(cd ${WORK_DIR} && INGEST_SIGSTRUCT_URI=$(curl -s https://enclave-distribution.${NAMESPACE}.mobilecoin.com/production.json | grep ingest-enclave.css | awk '{print $2}' | tr -d \" | tr -d ,)
-curl -O https://enclave-distribution.${NAMESPACE}.mobilecoin.com/${INGEST_SIGSTRUCT_URI})
-INGEST_ENCLAVE_CSS="$WORK_DIR/ingest-enclave.css"
+    mkdir -p ${WORK_DIR}
+    SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-$SCRIPT_DIR/build-fs.sh $NET
-cp SCRIPT_DIR/../target/release/full-service $WORK_DIR
+    (cd ${WORK_DIR} && INGEST_SIGSTRUCT_URI=$(curl -s https://enclave-distribution.${NAMESPACE}.mobilecoin.com/production.json | grep ingest-enclave.css | awk '{print $2}' | tr -d \" | tr -d ,)
+    curl -O https://enclave-distribution.${NAMESPACE}.mobilecoin.com/${INGEST_SIGSTRUCT_URI})
+    INGEST_ENCLAVE_CSS="$WORK_DIR/ingest-enclave.css"
 
+    $SCRIPT_DIR/build-fs.sh $NET
+    cp SCRIPT_DIR/../target/release/full-service $WORK_DIR
+fi
 
 mkdir -p ${WALLET_DB_DIR}
-${FS_DIR}/full-service \
+$WORK_DIR/full-service \
     --wallet-db ${WALLET_DB_DIR}/wallet.db \
     --ledger-db ${LEDGER_DB_DIR} \
     --peer mc://node1.$NAMESPACE.mobilecoinww.com/ \
