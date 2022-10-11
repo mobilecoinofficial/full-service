49020e0e0ff3fa35a4d659cef5b7bc15c83f88fc
Pouneh Aghababazadeh <paghababazadeh@gmail.com>
2022-10-11 16:39:06 -0700

    Remove one layer of ifs

--- tools/build-fs.sh
+++ tools/build-fs.sh
@@ -9,42 +9,44 @@
 #   - CONSENSUS_ENCLAVE_CSS
 
 # Net can be main/test/local
-if [ $# -gt 0 ]; then
-    NET=$1
-fi
+NET="$1"
 
-if [ "$NET" == "test" ] || [ "$NET" == "main" ] || [ "$NET" == "alpha" ]; then
-    if [ $NET == "test" ]; then
-        NAMESPACE="test"
-        export SGX_MODE=HW
-        export IAS_MODE=PROD
-        CONSENSUS_SIGSTRUCT_URI=$(curl -s https://enclave-distribution.${NAMESPACE}.mobilecoin.com/production.json | grep consensus-enclave.css | awk '{print $2}' | tr -d \" | tr -d ,)
-    elif [ $NET == "main" ]; then
-        NAMESPACE="prod"
-        export SGX_MODE=HW
-        export IAS_MODE=PROD
-        CONSENSUS_SIGSTRUCT_URI=$(curl -s https://enclave-distribution.${NAMESPACE}.mobilecoin.com/production.json | grep consensus-enclave.css | awk '{print $2}' | tr -d \" | tr -d ,)
-    elif [ $NET == "alpha" ]; then
-        NAMESPACE="alpha"
-        export SGX_MODE=HW
-        export IAS_MODE=DEV
-        CONSENSUS_SIGSTRUCT_URI=""
-    fi 
-    
-    WORK_DIR="$HOME/.mobilecoin/${NET}"
-    export CONSENSUS_ENCLAVE_CSS="$WORK_DIR/consensus-enclave.css"
-    mkdir -p ${WORK_DIR}
-    
-    if ! test -f "$CONSENSUS_ENCLAVE_CSS" && [ $CONSENSUS_SIGSTRUCT_URI != ""]; then
-        (cd ${WORK_DIR} && curl -O https://enclave-distribution.${NAMESPACE}.mobilecoin.com/${CONSENSUS_SIGSTRUCT_URI})
-    fi
-
-    if ! test -f "$CONSENSUS_ENCLAVE_CSS"; then
-        echo "Missing consensus enclave at $CONSENSUS_ENCLAVE_CSS"
-        exit 1
-    fi
+if [ "$NET" == "test" ]; then
+    NAMESPACE="test"
+    export SGX_MODE=HW
+    export IAS_MODE=PROD
+    CONSENSUS_SIGSTRUCT_URI=$(curl -s https://enclave-distribution.${NAMESPACE}.mobilecoin.com/production.json | grep consensus-enclave.css | awk '{print $2}' | tr -d \" | tr -d ,)
+elif [ "$NET" == "main" ]; then
+    NAMESPACE="prod"
+    export SGX_MODE=HW
+    export IAS_MODE=PROD
+    CONSENSUS_SIGSTRUCT_URI=$(curl -s https://enclave-distribution.${NAMESPACE}.mobilecoin.com/production.json | grep consensus-enclave.css | awk '{print $2}' | tr -d \" | tr -d ,)
+elif [ "$NET" == "alpha" ]; then
+    NAMESPACE="alpha"
+    export SGX_MODE=HW
+    export IAS_MODE=DEV
+    CONSENSUS_SIGSTRUCT_URI=""
 else
     echo "Using current environment's SGX_MODE, IAS_MODE, CONSENSUS_ENCLAVE_CSS"
+    if [ "$NET" == "" ]; then
+        NET="default"
+fi 
+    
+WORK_DIR="$HOME/.mobilecoin/${NET}"
+CONSENSUS_DOWNLOAD_LOCATION="$WORK_DIR/consensus-enclave.css"
+mkdir -p ${WORK_DIR}
+    
+if ! test -f "$CONSENSUS_DOWNLOAD_LOCATION" && [ $CONSENSUS_SIGSTRUCT_URI != ""]; then
+    (cd ${WORK_DIR} && curl -O https://enclave-distribution.${NAMESPACE}.mobilecoin.com/${CONSENSUS_SIGSTRUCT_URI})
+fi
+
+if [ -z "$CONSENSUS_ENCLAVE_CSS" ]; then
+    export CONSENSUS_ENCLAVE_CSS=$CONSENSUS_DOWNLOAD_LOCATION
+fi
+
+if ! test -f "$CONSENSUS_ENCLAVE_CSS"; then
+    echo "Missing consensus enclave at $CONSENSUS_ENCLAVE_CSS"
+    exit 1
 fi
 
 echo "building full service..."
