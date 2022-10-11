3158c43e3c1c4e0fc7bafae69f1853f2017d8ca7
Pouneh Aghababazadeh <paghababazadeh@gmail.com>
2022-10-11 16:24:11 -0700

    Simplify build-fs logic

--- tools/build-fs.sh
+++ tools/build-fs.sh
@@ -18,32 +18,33 @@ if [ "$NET" == "test" ] || [ "$NET" == "main" ] || [ "$NET" == "alpha" ]; then
         NAMESPACE="test"
         export SGX_MODE=HW
         export IAS_MODE=PROD
+        CONSENSUS_SIGSTRUCT_URI=$(curl -s https://enclave-distribution.${NAMESPACE}.mobilecoin.com/production.json | grep consensus-enclave.css | awk '{print $2}' | tr -d \" | tr -d ,)
     elif [ $NET == "main" ]; then
         NAMESPACE="prod"
         export SGX_MODE=HW
         export IAS_MODE=PROD
+        CONSENSUS_SIGSTRUCT_URI=$(curl -s https://enclave-distribution.${NAMESPACE}.mobilecoin.com/production.json | grep consensus-enclave.css | awk '{print $2}' | tr -d \" | tr -d ,)
     elif [ $NET == "alpha" ]; then
         NAMESPACE="alpha"
         export SGX_MODE=HW
         export IAS_MODE=DEV
+        CONSENSUS_SIGSTRUCT_URI=""
     fi 
     
     WORK_DIR="$HOME/.mobilecoin/${NET}"
-    mkdir -p ${WORK_DIR}
     export CONSENSUS_ENCLAVE_CSS="$WORK_DIR/consensus-enclave.css"
-    if [ "$NET" == "alpha" ]; then
-        if ! test -f "$CONSENSUS_ENCLAVE_CSS"; then
-            if test -f "$WORK_DIR/libconsensus-enclave.css"; then
-                export CONSENSUS_ENCLAVE_CSS="$WORK_DIR/libconsensus-enclave.css"
-            else
-                echo "Please place the consensus enclave for alphanet in $WORK_DIR"
-                echo "Ask ops for the consensus and ingest enclaves."
-            fi
-        fi
-    else
-        (cd ${WORK_DIR} && CONSENSUS_SIGSTRUCT_URI=$(curl -s https://enclave-distribution.${NAMESPACE}.mobilecoin.com/production.json | grep consensus-enclave.css | awk '{print $2}' | tr -d \" | tr -d ,)
-            curl -O https://enclave-distribution.${NAMESPACE}.mobilecoin.com/${CONSENSUS_SIGSTRUCT_URI})
+    mkdir -p ${WORK_DIR}
+
+    
+    if ! test -f "$CONSENSUS_ENCLAVE_CSS" && [ $CONSENSUS_SIGSTRUCT_URI != ""]; then
+        (cd ${WORK_DIR} && curl -O https://enclave-distribution.${NAMESPACE}.mobilecoin.com/${CONSENSUS_SIGSTRUCT_URI})
     fi
+
+    if ! test -f "$CONSENSUS_ENCLAVE_CSS"; then
+        echo "Missing consensus enclave at $CONSENSUS_ENCLAVE_CSS"
+        exit 1
+    fi
+
 else
     echo "Using current environment's SGX_MODE, IAS_MODE, CONSENSUS_ENCLAVE_CSS"
 fi
