e8358d6b0948580766564284398c384a4f7bc02a
Pouneh Aghababazadeh <paghababazadeh@gmail.com>
2022-10-11 14:19:39 -0700

    Enable build-fs to also build for alphanet

--- tools/build-fs.sh
+++ tools/build-fs.sh
@@ -13,7 +13,7 @@ if [ $# -gt 0 ]; then
     NET=$1
 fi
 
-if [ "$NET" == "test" ] || [ "$NET" == "main" ]; then
+if [ "$NET" == "test" ] || [ "$NET" == "main" ] || [ "$NET" == "alpha" ]; then
     if [ $NET == "test" ]; then
         NAMESPACE="test"
         export SGX_MODE=HW
@@ -22,13 +22,28 @@ if [ "$NET" == "test" ] || [ "$NET" == "main" ]; then
         NAMESPACE="prod"
         export SGX_MODE=HW
         export IAS_MODE=PROD
+    elif [ $NET == "alpha" ]; then
+        NAMESPACE="alpha"
+        export SGX_MODE=HW
+        export IAS_MODE=DEV
     fi 
     
     WORK_DIR="$HOME/.mobilecoin/${NET}"
     mkdir -p ${WORK_DIR}
     export CONSENSUS_ENCLAVE_CSS="$WORK_DIR/consensus-enclave.css"
-    (cd ${WORK_DIR} && CONSENSUS_SIGSTRUCT_URI=$(curl -s https://enclave-distribution.${NAMESPACE}.mobilecoin.com/production.json | grep consensus-enclave.css | awk '{print $2}' | tr -d \" | tr -d ,)
-        curl -O https://enclave-distribution.${NAMESPACE}.mobilecoin.com/${CONSENSUS_SIGSTRUCT_URI})
+    if [ "$NET" == "alpha" ]; then
+        if ! test -f "$CONSENSUS_ENCLAVE_CSS"; then
+            if test -f "$WORK_DIR/libconsensus-enclave.css"; then
+                export CONSENSUS_ENCLAVE_CSS="$WORK_DIR/libconsensus-enclave.css"
+            else
+                echo "Please place the consensus enclave for alphanet in $WORK_DIR"
+                echo "Ask ops for the consensus and ingest enclaves."
+            fi
+        fi
+    else
+        (cd ${WORK_DIR} && CONSENSUS_SIGSTRUCT_URI=$(curl -s https://enclave-distribution.${NAMESPACE}.mobilecoin.com/production.json | grep consensus-enclave.css | awk '{print $2}' | tr -d \" | tr -d ,)
+            curl -O https://enclave-distribution.${NAMESPACE}.mobilecoin.com/${CONSENSUS_SIGSTRUCT_URI})
+    fi
 else
     echo "Using current environment's SGX_MODE, IAS_MODE, CONSENSUS_ENCLAVE_CSS"
 fi
