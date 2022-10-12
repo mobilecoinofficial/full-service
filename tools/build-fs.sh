6413c2cdbac82c03ed4ae63060a792376ababc59
p <paghababazadeh@gmail.com>
2022-10-11 18:19:08 -0700

    Run script  (#508)
    * Create a script that allows running fs for a specified network. by default, the script will also build Full Service unless otherwise specified
    
    * Adjust the build script to also build for alphanet
    
    Co-authored-by: Eran Rundstein <eran@rundste.in>

--- tools/build-fs.sh
+++ tools/build-fs.sh
@@ -9,30 +9,47 @@
 #   - CONSENSUS_ENCLAVE_CSS
 
 # Net can be main/test/local
-if [ $# -gt 0 ]; then
-    NET=$1
-fi
+NET="$1"
 
-if [ "$NET" == "test" ] || [ "$NET" == "main" ]; then
-    if [ $NET == "test" ]; then
-        NAMESPACE="test"
-        export SGX_MODE=HW
-        export IAS_MODE=PROD
-    elif [ $NET == "main" ]; then
-        NAMESPACE="prod"
-        export SGX_MODE=HW
-        export IAS_MODE=PROD
-    fi 
-    
-    WORK_DIR="$HOME/.mobilecoin/${NET}"
-    mkdir -p ${WORK_DIR}
-    export CONSENSUS_ENCLAVE_CSS="$WORK_DIR/consensus-enclave.css"
-    (cd ${WORK_DIR} && CONSENSUS_SIGSTRUCT_URI=$(curl -s https://enclave-distribution.${NAMESPACE}.mobilecoin.com/production.json | grep consensus-enclave.css | awk '{print $2}' | tr -d \" | tr -d ,)
-        curl -O https://enclave-distribution.${NAMESPACE}.mobilecoin.com/${CONSENSUS_SIGSTRUCT_URI})
+if [ "$NET" == "test" ]; then
+    NAMESPACE="test"
+    export SGX_MODE=HW
+    export IAS_MODE=PROD
+    CONSENSUS_SIGSTRUCT_URI=$(curl -s https://enclave-distribution.${NAMESPACE}.mobilecoin.com/production.json | grep consensus-enclave.css | awk '{print $2}' | tr -d \" | tr -d ,)
+elif [ "$NET" == "main" ]; then
+    NAMESPACE="prod"
+    export SGX_MODE=HW
+    export IAS_MODE=PROD
+    CONSENSUS_SIGSTRUCT_URI=$(curl -s https://enclave-distribution.${NAMESPACE}.mobilecoin.com/production.json | grep consensus-enclave.css | awk '{print $2}' | tr -d \" | tr -d ,)
+elif [ "$NET" == "alpha" ]; then
+    NAMESPACE="alpha"
+    export SGX_MODE=HW
+    export IAS_MODE=DEV
+    CONSENSUS_SIGSTRUCT_URI=""
 else
     echo "Using current environment's SGX_MODE, IAS_MODE, CONSENSUS_ENCLAVE_CSS"
+    CONSENSUS_SIGSTRUCT_URI=""
+    if [ "$NET" == "" ]; then
+        NET="default"
+    fi
+fi 
+    
+WORK_DIR="$HOME/.mobilecoin/${NET}"
+CONSENSUS_DOWNLOAD_LOCATION="$WORK_DIR/consensus-enclave.css"
+mkdir -p ${WORK_DIR}
+
+if ! test -f "$CONSENSUS_DOWNLOAD_LOCATION" && [ "$CONSENSUS_SIGSTRUCT_URI" != "" ]; then
+    (cd ${WORK_DIR} && curl -O https://enclave-distribution.${NAMESPACE}.mobilecoin.com/${CONSENSUS_SIGSTRUCT_URI})
 fi
 
+if [ -z "$CONSENSUS_ENCLAVE_CSS" ]; then
+    export CONSENSUS_ENCLAVE_CSS=$CONSENSUS_DOWNLOAD_LOCATION
+fi
+
+if ! test -f "$CONSENSUS_ENCLAVE_CSS"; then
+    echo "Missing consensus enclave at $CONSENSUS_ENCLAVE_CSS"
+    exit 1
+fi
 
 echo "building full service..."
 cargo build --release
---
+++ tools/run-fs.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+# Copyright (c) 2022 The MobileCoin Foundation
+
+NET="$1"
+
+if [ "$NET" == "main" ]; then
+    NAMESPACE="prod"
+    PEER_DOMAIN="prod.mobilecoinww.com/"
+    TX_SOURCE_URL="https://ledger.mobilecoinww.com"
+    INGEST_SIGSTRUCT_URI=$(curl -s https://enclave-distribution.${NAMESPACE}.mobilecoin.com/production.json | grep ingest-enclave.css | awk '{print $2}' | tr -d \" | tr -d ,)
+elif [ "$NET" == "test" ]; then
+    NAMESPACE=$NET
+    PEER_DOMAIN="test.mobilecoin.com/"
+    TX_SOURCE_URL="https://s3-us-west-1.amazonaws.com/mobilecoin.chain"
+    INGEST_SIGSTRUCT_URI=$(curl -s https://enclave-distribution.${NAMESPACE}.mobilecoin.com/production.json | grep ingest-enclave.css | awk '{print $2}' | tr -d \" | tr -d ,)
+elif [ "$NET" == "alpha" ]; then
+    NAMESPACE=$NET
+    PEER_DOMAIN="alpha.development.mobilecoin.com/"
+    TX_SOURCE_URL="https://s3-eu-central-1.amazonaws.com/mobilecoin.eu.development.chain"
+    INGEST_SIGSTRUCT_URI=""
+else
+    # TODO: add support for local network
+    echo "Unknown network"
+    echo "Usage: run-fs.sh {main|test|alpha} [--no-build]"
+    exit 1
+fi
+
+WORK_DIR="$HOME/.mobilecoin/${NET}"
+WALLET_DB_DIR="${WORK_DIR}/wallet-db"
+LEDGER_DB_DIR="${WORK_DIR}/ledger-db"
+INGEST_DOWNLOAD_LOCATION="$WORK_DIR/ingest-enclave.css"
+mkdir -p ${WORK_DIR}
+
+
+if ! test -f "$INGEST_DOWNLOAD_LOCATION" && [ "$INGEST_SIGSTRUCT_URI" != "" ]; then
+    (cd ${WORK_DIR} && curl -O https://enclave-distribution.${NAMESPACE}.mobilecoin.com/${INGEST_SIGSTRUCT_URI})
+fi
+
+if [ -z "$INGEST_ENCLAVE_CSS" ]; then
+    export INGEST_ENCLAVE_CSS=$INGEST_DOWNLOAD_LOCATION
+fi
+
+if ! test -f "$INGEST_ENCLAVE_CSS"; then
+    echo "Missing ingest enclave at $INGEST_ENCLAVE_CSS"
+    exit 1
+fi
+
+# Pass "--no-build" if the user just wants to run what they have in  
+# WORK_DIR instead of building and copying over a new exectuable
+if [ "$2" != "--no-build" ]; then
+    echo "Building"
+    SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+    $SCRIPT_DIR/build-fs.sh $NET
+    cp $SCRIPT_DIR/../target/release/mc-full-service $WORK_DIR
+fi 
+
+mkdir -p ${WALLET_DB_DIR}
+$WORK_DIR/mc-full-service \
+    --wallet-db ${WALLET_DB_DIR}/wallet.db \
+    --ledger-db ${LEDGER_DB_DIR} \
+    --peer mc://node1.${PEER_DOMAIN} \
+    --peer mc://node2.${PEER_DOMAIN} \
+    --tx-source-url ${TX_SOURCE_URL}/node1.${PEER_DOMAIN} \
+    --tx-source-url ${TX_SOURCE_URL}/node2.${PEER_DOMAIN}  \
+    --fog-ingest-enclave-css $INGEST_ENCLAVE_CSS \
+    --chain-id $NET
