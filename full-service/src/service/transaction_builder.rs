2a40b698cb374e82a97d22113bcf0df8a77bc33c
Pouneh Aghababazadeh <paghababazadeh@gmail.com>
2022-10-18 11:14:55 -0700

    WIP: use ledger to get the txo

--- full-service/src/service/transaction_builder.rs
+++ full-service/src/service/transaction_builder.rs
@@ -277,7 +277,9 @@ impl<FPR: FogPubkeyResolver + 'static> WalletTransactionBuilder<FPR> {
             .inputs
             .iter()
             .map(|utxo| {
-                let txo: TxOut = mc_util_serial::decode(&utxo.txo)?;
+                let cristretto = utxo.public_key()?;
+                let txo_i = self.ledger_db.get_tx_out_index_by_public_key(&cristretto)?;
+                let txo = self.ledger_db.get_tx_out_by_index(txo_i)?;
                 self.ledger_db.get_tx_out_index_by_hash(&txo.hash())
             })
             .collect::<Result<Vec<u64>, mc_ledger_db::Error>>()?;
@@ -293,7 +295,9 @@ impl<FPR: FogPubkeyResolver + 'static> WalletTransactionBuilder<FPR> {
         let excluded_tx_out_indices: Vec<u64> = inputs_and_proofs
             .iter()
             .map(|(utxo, _membership_proof)| {
-                let txo: TxOut = mc_util_serial::decode(&utxo.txo)?;
+                let cristretto = utxo.public_key()?;
+                let txo_i = self.ledger_db.get_tx_out_index_by_public_key(&cristretto)?;
+                let txo = self.ledger_db.get_tx_out_by_index(txo_i)?;
                 self.ledger_db
                     .get_tx_out_index_by_hash(&txo.hash())
                     .map_err(WalletTransactionBuilderError::LedgerDB)
@@ -321,8 +325,9 @@ impl<FPR: FogPubkeyResolver + 'static> WalletTransactionBuilder<FPR> {
             let subaddress_index = utxo.subaddress_index.ok_or_else(|| {
                 WalletTransactionBuilderError::CannotUseOrphanedTxoAsInput(utxo.id.clone())
             })?;
-
-            let db_tx_out: TxOut = mc_util_serial::decode(&utxo.txo)?;
+            let cristretto = utxo.public_key()?;
+            let txo_i = self.ledger_db.get_tx_out_index_by_public_key(&cristretto)?;
+            let db_tx_out = self.ledger_db.get_tx_out_by_index(txo_i)?;
 
             let (mut ring, mut membership_proofs) = rings_and_proofs
                 .pop()
