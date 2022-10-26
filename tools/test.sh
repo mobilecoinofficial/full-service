d54e00970b0e86a131eef026d0bd308b03c62f8c
Brian <brian.william.corbin@gmail.com>
2022-10-26 10:48:05 -0700

    adjusting test scripts

--- tools/test.sh
+++ tools/test.sh
@@ -11,22 +11,6 @@ if [[ ! -z "$1" ]]; then
 fi
 
 echo "Testing in $PWD"
-CARGO_INCREMENTAL=0 RUSTFLAGS='-Cinstrument-coverage' LLVM_PROFILE_FILE="../target/profraw/json5format-%m.profraw" \
 SGX_MODE=SW IAS_MODE=DEV CONSENSUS_ENCLAVE_CSS=$(pwd)/consensus-enclave.css \
 cargo test
 echo "Testing in $PWD complete."
-
-echo "Building coverage report (lcov) html to target/coverage/results"
-mkdir -p ./target/coverage
-grcov . --binary-path ./target/debug/deps/ --source-dir . -t lcov --branch --ignore-not-existing \
---ignore "../*" --ignore "/*" --ignore "mobilecoin/*" --ignore "target/*" --ignore "*/e2e_tests/*" \
--o ./target/coverage/tests.lcov
-genhtml ./target/coverage/tests.lcov --show-details --prefix "$PWD" --output-directory ./target/coverage/results
-
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  open ./target/coverage/results/index.html
-#elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  # ...
-else
-  echo "test output written to target/coverage/results/index.html"
-fi
