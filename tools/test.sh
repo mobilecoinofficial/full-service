4b994ec93373a5f4e7d82aa2fab7d06a87dd7d84
Brian <brian.william.corbin@gmail.com>
2022-11-07 17:42:45 -0800

    updating test script

--- tools/test.sh
+++ tools/test.sh
@@ -2,15 +2,7 @@
 
 # Copyright (c) 2018-2020 MobileCoin Inc.
 
-# RUSTFLAGS="-C instrument-coverage" \
-
-set -e
-
-if [[ ! -z "$1" ]]; then
-    cd "$1"
-fi
-
 echo "Testing in $PWD"
 SGX_MODE=SW IAS_MODE=DEV CONSENSUS_ENCLAVE_CSS=$(pwd)/consensus-enclave.css \
-cargo test test_balance_sent_to_
+cargo test $1
 echo "Testing in $PWD complete."
