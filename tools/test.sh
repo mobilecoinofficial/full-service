79a8ad52b3990252cb07fde534e18fb2312da3ac
Brian <brian.william.corbin@gmail.com>
2022-11-08 06:52:10 -0800

    updating test script

--- tools/test.sh
+++ tools/test.sh
@@ -4,13 +4,7 @@
 
 # RUSTFLAGS="-C instrument-coverage" \
 
-set -e
-
-if [[ ! -z "$1" ]]; then
-    cd "$1"
-fi
-
 echo "Testing in $PWD"
 SGX_MODE=SW IAS_MODE=DEV CONSENSUS_ENCLAVE_CSS=$(pwd)/consensus-enclave.css \
-cargo test
+cargo test $1
 echo "Testing in $PWD complete."
