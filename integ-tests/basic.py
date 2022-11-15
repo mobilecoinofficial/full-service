fa112fb39267ad6ff3cbd8384081ead94303278f
Pouneh Aghababazadeh <paghababazadeh@gmail.com>
2022-11-15 13:37:22 -0800

    Import an account

--- integ-tests/basic.py
+++ integ-tests/basic.py
@@ -7,26 +7,61 @@
 #     get_wallet_status
 #     build, build_and_submit, build_split_txo .. etc
 
-import sys
 import os
+import sys
+
 sys.path.append(os.path.abspath("../cli"))
 
-from fullservice import FullServiceAPIv2 as v2 
 import asyncio
 import json
-import subprocess
-from pathlib import Path
+
+from fullservice import FullServiceAPIv2 as v2
 
 with open('config') as json_file:
     config = json.load(json_file)
 
+def get_mnemonics(n = 2):
+    if n > len(config['Account Mnemonics']):
+        raise ValueError("Not enough account available in config")
+
+    return config['Account Mnemonics'][:n]
 
 async def main():
     fs = v2()
-
     network_status = await fs.get_network_status()
-    print(network_status)
+
+    m = get_mnemonics()
+    alice = await fs.import_account(m[0]['mnemonic'], "2")
+    assert('error' not in alice.keys())
+    alice = alice['result']['account']
+
+
+    return    
+    alice = fs.import_account().result.account
+    alice = fs.get_account_status(alice.account_id).result
+    bob = fs.import_account().result.account
+    bob = fs.get_account_status(bob.account_id).result
+
+
+    first_transaction = fs.build_and_submit_transaction(
+        alice.account.account_id,
+        recipient_public_address = bob.account.main_address,
+        amount = { "value" : str(1), "token_id": str(0)}
+    ).result
+
+
+    total_spent = first_transaction.transactionlog.fee_value + first_transaction.payload_txos[0].value
+    alice2 = fs.get_account_status(alice.account.account_id).result
+    assert(alice2.balance_per_token["0"].unspent == 
+           alice.balance_per_token["0"].unspent - total_spent)
+
     
+    log = fs.get_transaction_log(first_transaction.transaction_log.id).result
+    assert(log.status == "tx_status_succeeded")
+    
+
+    existing_accounts = await fs.get_accounts()
+    print(existing_accounts)
 
 if __name__ == '__main__':
     asyncio.run(main())
