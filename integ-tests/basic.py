bc6e52d8799ce1968eb096976b25cf8b00358cb1
Pouneh Aghababazadeh <paghababazadeh@gmail.com>
2022-11-28 16:00:18 -0800

    Rename dataobjects to FSDataObjects

--- integ-tests/basic.py
+++ integ-tests/basic.py
@@ -16,7 +16,7 @@ import json
 sys.path.append(os.path.abspath("../cli"))
 
 from fullservice import FullServiceAPIv2 as v2
-from dataobjects import Response, Account  # TODO rename as FSDataObjects
+from FSDataObjects import Response, Account 
 
 default_config_path = "./config"
 config = []
