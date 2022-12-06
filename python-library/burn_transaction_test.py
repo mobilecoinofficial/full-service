620e8788eb594a2e5085f621cce6e42a61236959
cxloe <77425619+cxloe@users.noreply.github.com>
2022-12-06 17:47:13 -0500

    initial stab at importing things seperately, test skeleton

---
+++ python-library/burn_transaction_test.py
@@ -0,0 +1,28 @@
+import argparse
+import asyncio
+import json
+import subprocess
+import sys
+from fullservice import FullServiceAPIv2 as v2
+from FSDataObjects import Response, Account 
+
+repo_root_dir = subprocess.check_output("git rev-parse --show-toplevel", shell=True).decode("utf8").strip()
+sys.path.append("{}/.internal-ci/test/fs-integration".format(repo_root_dir))
+
+import basic as itf # import basic as the integration test framework. this should live in a different file.
+
+
+fs = v2()
+
+async def import_accounts():
+    itf.get_mnemonics()
+    await itf.get_account()
+
+async def test_burn_transaction():
+    await import_accounts()
+    alice = await itf.get_account(0, "alice", True)
+    bob = await itf.get_account(1, "bob", True)
+    burn_tx = await fs.build_burn_transaction(alice.id, "400")
+
+
+asyncio.run(test_burn_transaction())
