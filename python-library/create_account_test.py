04d41251d72f7571d6d734f0d9ef6905683bb3ab
cxloe <77425619+cxloe@users.noreply.github.com>
2022-12-05 20:03:29 -0500

    black

--- python-library/create_account_test.py
+++ python-library/create_account_test.py
@@ -1,14 +1,17 @@
-import asyncio 
+import asyncio
 from fullservice import FullServiceAPIv2 as v2
+
 
 async def main():
     fs = v2()
     accounts_before = await fs.get_accounts()
-    count = accounts_before['result']['account_ids']
+    count = accounts_before["result"]["account_ids"]
     await fs.create_account()
     accounts_after = await fs.get_accounts()
-    if len(accounts_after['result']['account_ids']) == len(count) + 1:
+    if len(accounts_after["result"]["account_ids"]) == len(count) + 1:
         print("Account created successfully!")
     else:
         print("Account creation failed")
+
+
 asyncio.run(main())
