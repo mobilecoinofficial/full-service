8c025f64cb530d2fa78c3089d4e6952661e36baa
cxloe <77425619+cxloe@users.noreply.github.com>
2022-12-07 16:10:05 -0500

    import accounts func

--- python-library/burn_transaction_test.py
+++ python-library/burn_transaction_test.py
@@ -10,19 +10,20 @@ repo_root_dir = subprocess.check_output("git rev-parse --show-toplevel", shell=T
 sys.path.append("{}/.internal-ci/test/fs-integration".format(repo_root_dir))
 
 import basic as itf # import basic as the integration test framework. this should live in a different file.
-from basic import TestUtils
+from basic import TestUtils as Utils
+import export_check_all as account_tools
 
 fs = v2()
 
 async def import_accounts():
-    TestUtils.get_mnemonics()
-    await itf.init_test_accounts()
+    Utils.get_mnemonics()
+    alice = await itf.init_test_accounts(0, "alice", True)
+    bob = await itf.init_test_account(1, "bob", True)
+    burn_tx = await fs.build_burn_transaction(alice.id, "400")
 
 async def test_burn_transaction():
     await import_accounts()
-    alice = await itf.get_account(0, "alice", True)
-    bob = await itf.get_account(1, "bob", True)
-    burn_tx = await fs.build_burn_transaction(alice.id, "400")
+   
 
 
 asyncio.run(test_burn_transaction())
