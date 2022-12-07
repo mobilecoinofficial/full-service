c903150cefee6227b5bbb68c59de0b96708327fd
cxloe <77425619+cxloe@users.noreply.github.com>
2022-12-07 16:31:19 -0500

    build the burn transaction

--- python-library/burn_transaction_test.py
+++ python-library/burn_transaction_test.py
@@ -4,25 +4,36 @@ import json
 import subprocess
 import sys
 from fullservice import FullServiceAPIv2 as v2
-from FSDataObjects import Response, Account 
+from FSDataObjects import Response, Account
 
-repo_root_dir = subprocess.check_output("git rev-parse --show-toplevel", shell=True).decode("utf8").strip()
-sys.path.append("{}/.internal-ci/test/fs-integration".format(repo_root_dir))
-
-import basic as itf # import basic as the integration test framework. this should live in a different file.
+repo_root_dir = (
+    subprocess.check_output("git rev-parse --show-toplevel", shell=True)
+    .decode("utf8")
+    .strip()
+)
+sys.path.append(
+    "{}/.internal-ci/test/fs-integration".format(repo_root_dir)
+)  # we're importing the basic.py file as the integration test framework
+import basic as itf
 from basic import TestUtils as Utils
-import export_check_all as account_tools # this will be folded into the integration test framework
+
+import export_check_all as account_tools  # this will be folded into the integration test framework
 
 fs = v2()
 
-async def test_burn_transaction():
+
+async def test_burn_transaction(amount_pmob: int = 600000000):
     # await account_tools.clean()
     Utils.get_mnemonics()
     alice = await itf.init_test_accounts(0, "alice", True)
     bob = await itf.init_test_accounts(1, "bob", True)
-    burn_tx = await fs.build_burn_transaction(alice.id, "400")
+    burn_tx = await fs.build_and_submit_transaction(
+        alice.id,
+        recipient_public_address=bob.main_address,
+        amount={"value": str(amount_pmob), "token_id": str(0)},
+    )
+
     await Utils.wait_for_network_sync()
-    
 
 
 asyncio.run(test_burn_transaction())
