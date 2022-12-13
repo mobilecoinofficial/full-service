0ee7df47bc0af1ae9a0d6d17021062916c1c2e7d
cxloe <77425619+cxloe@users.noreply.github.com>
2022-12-13 18:24:02 -0500

    transaction signer lib initial proof of concept

--- python-library/test_signer_burn_transaction.py
+++ python-library/test_signer_burn_transaction.py
@@ -3,7 +3,8 @@ import subprocess
 import sys
 from fullservice import FullServiceAPIv2 as v2
 import pytest
-
+import os
+import transaction_signer_lib
 
 repo_root_dir = (
     subprocess.check_output("git rev-parse --show-toplevel", shell=True)
@@ -23,34 +24,39 @@ async def test_burn_transaction(amount_pmob: int = 600000000):
     utils = Utils()
     Utils.get_mnemonics()
     alice = await utils.init_test_accounts(0, "alice", True)
+    alice_export = await fs.export_account_secrets(alice.id)
+    entropy = alice_export.get("result").get("account_secrets").get("mnemonic").removeprefix('(').removesuffix(')')
 
-    balance_before = int(
-        (await fs.get_account_status(alice.id))
-        .get("result")
-        .get("balance_per_token")
-        .get("0")
-        .get("unspent")
-    )
+    transaction_signer_lib.create_account(name="alice", mnemonic=entropy)
 
-    burn_tx = await fs.build_burn_transaction(
-        alice.id,
-        amount={"value": str(amount_pmob), "token_id": str(0)},
-    )
+    # balance_before = int(
+    #     (await fs.get_account_status(alice.id))
+    #     .get("result")
+    #     .get("balance_per_token")
+    #     .get("0")
+    #     .get("unspent")
+    # )
+    # os.system("../target/release/transaction-signer import ")
 
-    print(type(burn_tx.get("result").get("tx_proposal")))
+    # unsigned_burn_tx = await fs.build_unsigned_burn_transaction(
+    #     alice.id,
+    #     amount={"value": str(amount_pmob), "token_id": str(0)},
+    # )
 
-    await fs.submit_transaction(burn_tx.get("result").get("tx_proposal"))
-    await utils.wait_two_blocks()
-    balance_after = int(
-        (await fs.get_account_status(alice.id))
-        .get("result")
-        .get("balance_per_token")
-        .get("0")
-        .get("unspent")
-    )
-    print(balance_before, balance_after)
-    assert balance_before > balance_after, "Burn transaction failed"
-    exit
+    # print(type(unsigned_burn_tx.get("result").get("tx_proposal")))
+
+    # await fs.submit_transaction(burn_tx.get("result").get("tx_proposal"))
+    # await utils.wait_two_blocks()
+    # balance_after = int(
+    #     (await fs.get_account_status(alice.id))
+    #     .get("result")
+    #     .get("balance_per_token")
+    #     .get("0")
+    #     .get("unspent")
+    # )
+    # print(balance_before, balance_after)
+    # assert balance_before > balance_after, "Burn transaction failed"
+    # exit
 
 
 asyncio.run(test_burn_transaction())
