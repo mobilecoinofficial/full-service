ab83ad37aeda5a33a88c36031c4d706e13a8b68f
Eran Rundstein <eran@rundste.in>
2023-01-23 15:32:34 -0800

    A basic search endpoint (#669)
    * add basic search endpoint
    
    * include entire block and contents in search results
    
    * add tests
    
    * add doc
    
    * alphabetize
    
    * fmt

---
+++ full-service/src/json_rpc/v2/models/ledger.rs
@@ -0,0 +1,67 @@
+// Copyright (c) 2020-2021 MobileCoin Inc.
+
+//! API definition for ledger-related objects.
+
+use crate::{
+    json_rpc::v2::models::block::{Block, BlockContents},
+    service::models::ledger::LedgerSearchResult as ServiceLedgerSearchResult,
+};
+use serde_derive::{Deserialize, Serialize};
+
+// TxOut search result
+#[derive(Clone, Debug, Default, Deserialize, Serialize)]
+pub struct LedgerTxOutSearchResult {
+    pub block_contents_tx_out_index: String,
+    pub global_tx_out_index: String,
+}
+
+// KeyImage search result
+#[derive(Clone, Debug, Default, Deserialize, Serialize)]
+pub struct LedgerKeyImageSearchResult {
+    pub block_contents_key_image_index: String,
+}
+
+/// Information about a single search result
+#[derive(Clone, Debug, Default, Deserialize, Serialize)]
+pub struct LedgerSearchResult {
+    pub result_type: String,
+    pub block: Block,
+    pub block_contents: BlockContents,
+    pub tx_out: Option<LedgerTxOutSearchResult>,
+    pub key_image: Option<LedgerKeyImageSearchResult>,
+}
+
+impl From<&ServiceLedgerSearchResult> for LedgerSearchResult {
+    fn from(src: &ServiceLedgerSearchResult) -> Self {
+        match src {
+            ServiceLedgerSearchResult::TxOut {
+                block,
+                block_contents,
+                block_contents_tx_out_index,
+                tx_out_global_index,
+            } => Self {
+                result_type: "TxOut".to_string(),
+                block: block.into(),
+                block_contents: block_contents.into(),
+                tx_out: Some(LedgerTxOutSearchResult {
+                    block_contents_tx_out_index: block_contents_tx_out_index.to_string(),
+                    global_tx_out_index: tx_out_global_index.to_string(),
+                }),
+                ..Default::default()
+            },
+            ServiceLedgerSearchResult::KeyImage {
+                block,
+                block_contents,
+                block_contents_key_image_index,
+            } => Self {
+                result_type: "KeyImage".to_string(),
+                block: block.into(),
+                block_contents: block_contents.into(),
+                key_image: Some(LedgerKeyImageSearchResult {
+                    block_contents_key_image_index: block_contents_key_image_index.to_string(),
+                }),
+                ..Default::default()
+            },
+        }
+    }
+}
