ff99f2a5e806cc4fb46d8e5be7fa279e3169b243
Pouneh Aghababazadeh <paghababazadeh@gmail.com>
2023-02-08 11:52:10 -0800

    Undo script changes

--- tools/test.sh
+++ tools/test.sh
@@ -4,12 +4,6 @@
 
 # RUSTFLAGS="-C instrument-coverage" \
 
-
-if ! [[ -f "$(pwd)/consensus-enclave.css" ]]; then
-  echo "Please copy the consensus enclave to $(pwd)"
-  exit 1
-fi
-
 echo "Testing in $PWD"
 SGX_MODE=SW IAS_MODE=DEV CONSENSUS_ENCLAVE_CSS=$(pwd)/consensus-enclave.css \
 cargo test $1
