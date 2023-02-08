fb99dde5ce0b5101f74dd9679fbe66e83f680166
Pouneh Aghababazadeh <paghababazadeh@gmail.com>
2023-02-08 11:41:19 -0800

    Early exit on test script if missing consensus enclave

--- tools/test.sh
+++ tools/test.sh
@@ -4,6 +4,11 @@
 
 # RUSTFLAGS="-C instrument-coverage" \
 
+if ! [[ -f "$(pwd)/consensus-enclave.css" ]]; then
+  echo "Please copy the consensus enclave to $(pwd)"
+  exit 1
+fi
+
 echo "Testing in $PWD"
 SGX_MODE=SW IAS_MODE=DEV CONSENSUS_ENCLAVE_CSS=$(pwd)/consensus-enclave.css \
 cargo test $1
