29ff2591a05fdc9690a2c233aadc61fc9a950599
Jason Greathouse <jgreat@jgreat.me>
2023-04-12 10:38:35 -0500

    deploy fsm for testing - 5

--- .internal-ci/docker/entrypoints/full-service.sh
+++ .internal-ci/docker/entrypoints/full-service.sh
@@ -13,6 +13,21 @@ echo "Starting Up with command ${1}"
 mkdir -p "${MC_LEDGER_DB}"
 mkdir -p "$(dirname "${MC_WALLET_DB}")"
 
+# Remove unset wallet db value so we can run in Read Only mode.
+if [[ -n "${MC_READ_ONLY}" ]]
+then
+    echo "MC_READ_ONLY set. Unset MC_WALLET_DB value."
+    unset MC_WALLET_DB
+fi
+
+# Unset MC_PEER and TX_SOURCE_URL if we're running with a validator. The validator will need the consensus config.
+if [[ -n "${MC_VALIDATOR}" ]]
+then
+    echo "MC_VALIDATOR set. Unset MC_PEER and MC_TX_SOURCE_URL."
+    unset MC_PEER
+    unset MC_TX_SOURCE_URL
+fi
+
 # Check to see if leading argument starts with "--".
 # If so execute with full-service for compatibility with the previous container/cli arg only configuration.
 if [[ "${1}" =~ ^--.* ]]
