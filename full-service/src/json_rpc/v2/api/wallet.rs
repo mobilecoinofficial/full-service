a1433370cb49f61f104e70ac06d884a995c7563a
Brian Corbin <brian@mobilecoin.com>
2023-04-13 12:16:23 -0700

    updating API to optionally take in private keys when importing VOAccount

--- full-service/src/json_rpc/v2/api/wallet.rs
+++ full-service/src/json_rpc/v2/api/wallet.rs
@@ -36,6 +36,7 @@ use crate::{
         address::AddressService,
         balance::BalanceService,
         confirmation_number::ConfirmationService,
+        hardware_wallet::get_view_only_account_keys,
         ledger::LedgerService,
         models::tx_proposal::TxProposal,
         payment_request::PaymentRequestService,
@@ -46,9 +47,12 @@ use crate::{
         watcher::WatcherService,
         WalletService,
     },
-    util::b58::{
-        b58_decode_payment_request, b58_encode_public_address, b58_printable_wrapper_type,
-        PrintableWrapperType,
+    util::{
+        b58::{
+            b58_decode_payment_request, b58_encode_public_address, b58_printable_wrapper_type,
+            PrintableWrapperType,
+        },
+        encoding_helpers::{hex_to_ristretto, hex_to_ristretto_public},
     },
 };
 use mc_account_keys::burn_address;
@@ -1118,8 +1122,41 @@ where
                 .transpose()
                 .map_err(format_error)?;
 
+            let (view_private_key, spend_public_key, managed_by_hardware_wallet) =
+                match (view_private_key, spend_public_key) {
+                    (Some(view_private_key), Some(spend_public_key)) => {
+                        let view_private_key =
+                            hex_to_ristretto(&view_private_key).map_err(format_error)?;
+                        let spend_public_key =
+                            hex_to_ristretto_public(&spend_public_key).map_err(format_error)?;
+
+                        (view_private_key.into(), spend_public_key.into(), false)
+                    }
+                    (None, None) => {
+                        let view_account =
+                            get_view_only_account_keys().await.map_err(format_error)?;
+                        (
+                            view_account.view_private_key().clone(),
+                            view_account.spend_public_key().clone(),
+                            true,
+                        )
+                    }
+                    _ => {
+                        return Err(format_error(
+                            "Must provide both view_private_key and spend_public_key, or neither",
+                        ))
+                    }
+                };
+
             let account = service
-                .import_view_only_account(view_private_key, spend_public_key, name, fb, ns)
+                .import_view_only_account(
+                    &view_private_key,
+                    &spend_public_key,
+                    name,
+                    fb,
+                    ns,
+                    managed_by_hardware_wallet,
+                )
                 .map_err(format_error)?;
             let next_subaddress_index = service
                 .get_next_subaddress_index_for_account(&AccountID(account.id.clone()))
