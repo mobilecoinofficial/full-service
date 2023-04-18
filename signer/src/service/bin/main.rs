f3dcb49c34e380f677b4684d302c9dc1f063f1de
Brian <brian.william.corbin@gmail.com>
2023-04-17 17:03:24 -0700

    first pass at implementing functionality

--- signer/src/service/bin/main.rs
+++ signer/src/service/bin/main.rs
@@ -1,8 +1,36 @@
+use std::{net::IpAddr, str::FromStr};
+
 use mc_signer::service::api::signer_service_api;
 
+use clap::Parser;
 use rocket::{self, launch, routes, Build, Rocket};
+
+/// Command line config for the Wallet API
+#[derive(Clone, Debug, Parser)]
+#[clap(
+    name = "signer-service",
+    about = "An HTTP signer service for MobileCoin",
+    version
+)]
+pub struct APIConfig {
+    /// Host to listen on.
+    #[clap(long, default_value = "127.0.0.1", env = "MC_SIGNER_LISTEN_HOST")]
+    pub listen_host: String,
+
+    /// Port to start webserver on.
+    #[clap(long, default_value = "9092", env = "MC_SIGNER_LISTEN_PORT")]
+    pub listen_port: u16,
+}
 
 #[launch]
 fn rocket() -> Rocket<Build> {
-    rocket::build().mount("/", routes![signer_service_api])
+    let config = APIConfig::parse();
+
+    let rocket_config = rocket::Config {
+        address: IpAddr::from_str(&config.listen_host).expect("failed parsing host"),
+        port: config.listen_port,
+        ..rocket::Config::default()
+    };
+
+    rocket::custom(rocket_config).mount("/", routes![signer_service_api])
 }
