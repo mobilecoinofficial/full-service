82938e63b09dc997bbec7dedd0844e322c3b62d3
Brian Corbin <brian@mobilecoin.com>
2023-04-18 15:48:03 -0700

    adding error handling

--- signer/src/service/api/mod.rs
+++ signer/src/service/api/mod.rs
@@ -1,9 +1,10 @@
 use mc_common::logger::global_log;
 use mc_full_service::json_rpc::{
     json_rpc_request::JsonRPCRequest,
-    json_rpc_response::{format_invalid_request_error, JsonRPCError, JsonRPCResponse},
+    json_rpc_response::{format_error, format_invalid_request_error, JsonRPCResponse},
 };
 
+use anyhow::{anyhow, Result};
 use rocket::{get, post, serde::json::Json};
 
 use crate::{
@@ -29,7 +30,7 @@ pub fn version() -> serde_json::Value {
 #[post("/api", format = "json", data = "<command>")]
 pub fn signer_service_api(
     command: Json<JsonRPCRequest>,
-) -> Result<Json<JsonRPCResponse<JsonCommandResponse>>, String> {
+) -> Json<JsonRPCResponse<JsonCommandResponse>> {
     let req: JsonRPCRequest = command.0.clone();
 
     let mut response: JsonRPCResponse<JsonCommandResponse> = JsonRPCResponse {
@@ -44,7 +45,7 @@ pub fn signer_service_api(
         Ok(request) => request,
         Err(error) => {
             response.error = Some(format_invalid_request_error(error));
-            return Ok(Json(response));
+            return Json(response);
         }
     };
 
@@ -55,16 +56,14 @@ pub fn signer_service_api(
         }
         Err(rpc_error) => {
             global_log::info!("Command failed with error: {:?}", rpc_error);
-            response.error = Some(rpc_error);
+            response.error = Some(format_error(rpc_error));
         }
     };
 
-    Ok(Json(response))
+    Json(response)
 }
 
-fn signer_service_api_inner(
-    command: JsonCommandRequest,
-) -> Result<JsonCommandResponse, JsonRPCError> {
+fn signer_service_api_inner(command: JsonCommandRequest) -> Result<JsonCommandResponse> {
     global_log::info!("Running command {:?}", command);
 
     let response = match command {
@@ -72,24 +71,27 @@ fn signer_service_api_inner(
             let (mnemonic, account_info) = service::create_account();
             JsonCommandResponse::create_account {
                 mnemonic: mnemonic.to_string(),
-                account_info: account_info,
+                account_info,
             }
         }
         JsonCommandRequest::get_account { mnemonic } => {
-            let account_info = service::get_account(&mnemonic);
-            JsonCommandResponse::get_account { info: account_info }
+            let account_info = service::get_account(&mnemonic)?;
+            JsonCommandResponse::get_account { account_info }
         }
         JsonCommandRequest::sign_tx {
             mnemonic,
             unsigned_tx,
         } => {
-            let signed_tx = service::sign_tx(&mnemonic, unsigned_tx.try_into().unwrap());
+            let signed_tx = service::sign_tx(
+                &mnemonic,
+                unsigned_tx.try_into().map_err(|e: String| anyhow!(e))?,
+            )?;
             JsonCommandResponse::sign_tx {
-                tx_proposal: (&signed_tx).try_into().unwrap(),
+                tx_proposal: (&signed_tx).try_into().map_err(|e: String| anyhow!(e))?,
             }
         }
         JsonCommandRequest::sync_txos { mnemonic, txos } => {
-            let txos_synced = service::sync_txos(&mnemonic, txos);
+            let txos_synced = service::sync_txos(&mnemonic, txos)?;
             JsonCommandResponse::sync_txos { txos_synced }
         }
     };
