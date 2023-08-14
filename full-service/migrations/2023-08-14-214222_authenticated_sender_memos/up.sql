3d7c30dac411976d5d8df0f89156e162680f7201
Christian Oudard <ChristianOudard@pm.me>
2023-08-14 15:54:59 -0600

    Add migration for authenticated sender memo table.

---
+++ full-service/migrations/2023-08-14-214222_authenticated_sender_memos/up.sql
@@ -0,0 +1,10 @@
+ALTER TABLE txos ADD COLUMN memo_type INTEGER NULL;
+
+CREATE TABLE authenticated_sender_memos (
+  txo_id INTEGER PRIMARY KEY NOT NULL,
+  sender_address_hash TEXT NOT NULL,
+  payment_request_id TEXT,
+  payment_intent_id TEXT,
+  validated BOOLEAN NOT NULL,
+  FOREIGN KEY (txo_id) REFERENCES txos(id)
+);
--- full-service/src/db/schema.rs
+++ full-service/src/db/schema.rs
@@ -24,6 +24,16 @@ table! {
 }
 
 table! {
+    authenticated_sender_memos (txo_id) {
+        txo_id -> Integer,
+        sender_address_hash -> Text,
+        payment_request_id -> Nullable<Text>,
+        payment_intent_id -> Nullable<Text>,
+        validated -> Bool,
+    }
+}
+
+table! {
     gift_codes (id) {
         id -> Integer,
         gift_code_b58 -> Text,
@@ -77,6 +87,8 @@ table! {
         spent_block_index -> Nullable<BigInt>,
         confirmation -> Nullable<Binary>,
         shared_secret -> Nullable<Binary>,
+        memo -> Binary,
+        memo_type -> Nullable<Integer>,
     }
 }
 
@@ -88,6 +100,7 @@ table! {
 }
 
 joinable!(assigned_subaddresses -> accounts (account_id));
+joinable!(authenticated_sender_memos -> txos (txo_id));
 joinable!(transaction_input_txos -> transaction_logs (transaction_log_id));
 joinable!(transaction_input_txos -> txos (txo_id));
 joinable!(transaction_logs -> accounts (account_id));
@@ -98,6 +111,7 @@ joinable!(txos -> accounts (account_id));
 allow_tables_to_appear_in_same_query!(
     accounts,
     assigned_subaddresses,
+    authenticated_sender_memos,
     gift_codes,
     transaction_input_txos,
     transaction_logs,
