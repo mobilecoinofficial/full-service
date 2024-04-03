34768b8d972c571fe176565ee9411108eecc441b
Henry Holtzman <henry@mobilecoin.com>
2024-04-02 21:35:48 -0700

    Update function parameters when calling mobilecoind::UnspentTxOut (#970)

--- full-service/src/json_rpc/v1/models/tx_proposal.rs
+++ full-service/src/json_rpc/v1/models/tx_proposal.rs
@@ -9,6 +9,7 @@ use crate::{
 use mc_common::HashMap;
 use mc_mobilecoind_json::data_types::{JsonOutlayV2, JsonTx, JsonUnspentTxOut};
 
+use mc_transaction_core::MemoPayload;
 use mc_transaction_extra::TxOutConfirmationNumber;
 use redact::{expose_secret, Secret};
 use serde_derive::{Deserialize, Serialize};
@@ -57,6 +58,7 @@ impl TryFrom<&TxProposalServiceModel> for mc_mobilecoind::payments::TxProposal {
                 attempted_spend_height: 0,
                 attempted_spend_tombstone: 0,
                 token_id: *input_txo.amount.token_id,
+                memo_payload: MemoPayload::default().into(),
             })
             .collect();
 
