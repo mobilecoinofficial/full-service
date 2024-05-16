6750576db251cad7a72fb094dce0f917955b8fb2
sugargoat <sugargoat@mobilecoin.com>
2024-05-15 22:16:11 -0700

    e2e test in good shape
    Signed-off-by: sugargoat <sugargoat@mobilecoin.com>

--- full-service/src/json_rpc/v2/e2e_tests/transaction/build_submit/build_and_submit_spend_from_subaddress.rs
+++ full-service/src/json_rpc/v2/e2e_tests/transaction/build_submit/build_and_submit_spend_from_subaddress.rs
@@ -8,11 +8,7 @@ mod e2e_transaction {
         db::account::AccountID,
         json_rpc::v2::{
             api::test_utils::{dispatch, setup},
-            models::{
-                amount::Amount as AmountJSON,
-                transaction_log::TransactionLog as TransactionLogJSON,
-                tx_proposal::TxProposal as TxProposalJSON,
-            },
+            models::tx_proposal::TxProposal as TxProposalJSON,
         },
         service::models::tx_proposal::TxProposal,
         test_utils::{add_block_to_ledger_db, add_block_with_tx, manually_sync_account, MOB},
@@ -20,15 +16,15 @@ mod e2e_transaction {
     };
 
     use mc_common::logger::{test_with_logger, Logger};
-    use mc_crypto_keys::CompressedRistrettoPublic;
     use mc_ledger_db::Ledger;
     use mc_rand::rand_core::RngCore;
-    use mc_transaction_core::{ring_signature::KeyImage, tokens::Mob, tx::TxOut, Token};
+    use mc_transaction_core::{ring_signature::KeyImage, tokens::Mob, Token};
 
     use rand::{rngs::StdRng, SeedableRng};
     use serde_json::json;
 
-    use std::convert::{TryFrom, TryInto};
+    use itertools::Itertools;
+    use std::convert::TryFrom;
 
     // The narrative for this test is that an exchange creates subaccounts for its
     // users, Alice, Bob, and Carol. They receive 100, 200, and 300 MOB
@@ -53,69 +49,41 @@ mod e2e_transaction {
         let account_obj = result.get("account").unwrap();
         let account_id = account_obj.get("id").unwrap().as_str().unwrap();
 
-        // Assign a block of subaddresses for Alice, Bob, and Carol
-        let body = json!({
-            "jsonrpc": "2.0",
-            "id": 1,
-            "method": "assign_address_for_account",
-            "params": {
-                "account_id": account_id,
-                "metadata": "Subaddress for Alice",
-            }
-        });
-        let res = dispatch(&client, body, &logger);
-        let result = res.get("result").unwrap();
-        let alice_address = result.get("address").unwrap();
-        let alice_b58_public_address = alice_address
-            .get("public_address_b58")
-            .unwrap()
-            .as_str()
-            .unwrap();
-        let alice_public_address = b58_decode_public_address(alice_b58_public_address).unwrap();
-
-        let body = json!({
-            "jsonrpc": "2.0",
-            "id": 1,
-            "method": "assign_address_for_account",
-            "params": {
-                "account_id": account_id,
-                "metadata": "Subaddress for Bob",
-            }
-        });
-        let res = dispatch(&client, body, &logger);
-        let result = res.get("result").unwrap();
-        let bob_address = result.get("address").unwrap();
-        let bob_b58_public_address = bob_address
-            .get("public_address_b58")
-            .unwrap()
-            .as_str()
-            .unwrap();
-        let bob_public_address = b58_decode_public_address(bob_b58_public_address).unwrap();
-
-        let body = json!({
-            "jsonrpc": "2.0",
-            "id": 1,
-            "method": "assign_address_for_account",
-            "params": {
-                "account_id": account_id,
-                "metadata": "Subaddress for Carol",
-            }
-        });
-        let res = dispatch(&client, body, &logger);
-        let result = res.get("result").unwrap();
-        let carol_address = result.get("address").unwrap();
-        let carol_b58_public_address = carol_address
-            .get("public_address_b58")
-            .unwrap()
-            .as_str()
-            .unwrap();
-        let carol_public_address = b58_decode_public_address(carol_b58_public_address).unwrap();
+        let (
+            (alice_public_address, alice_b58_public_address),
+            (bob_public_address, bob_b58_public_address),
+            (carol_public_address, carol_b58_public_address),
+        ) = [
+            "Subaddress for Alice",
+            "Subaddress for Bob",
+            "Subaddress for Carol",
+        ]
+        .iter()
+        .map(|metadata| {
+            let body = json!({
+                "jsonrpc": "2.0",
+                "id": 1,
+                "method": "assign_address_for_account",
+                "params": {
+                    "account_id": account_id,
+                    "metadata": metadata,
+                }
+            });
+            let res = dispatch(&client, body, &logger);
+            let result = res.get("result").unwrap();
+            let address = result.get("address").unwrap();
+            let b58_address = address.get("public_address_b58").unwrap().as_str().unwrap();
+            let public_address = b58_decode_public_address(b58_address).unwrap();
+            (public_address, b58_address.to_string())
+        })
+        .collect_tuple()
+        .unwrap();
 
         // Add a block with a txo for Alice
         add_block_to_ledger_db(
             &mut ledger_db,
             &vec![alice_public_address.clone()],
-            100_000_000_000_000, // 100.0 MOB
+            100 * MOB,
             &[KeyImage::from(rng.next_u64())],
             &mut rng,
         );
@@ -123,7 +91,7 @@ mod e2e_transaction {
         add_block_to_ledger_db(
             &mut ledger_db,
             &vec![bob_public_address.clone()],
-            200_000_000_000_000, // 200.0 MOB
+            200 * MOB,
             &[KeyImage::from(rng.next_u64())],
             &mut rng,
         );
@@ -131,7 +99,7 @@ mod e2e_transaction {
         add_block_to_ledger_db(
             &mut ledger_db,
             &vec![carol_public_address.clone()],
-            300_000_000_000_000, // 300.0 MOB
+            300 * MOB,
             &[KeyImage::from(rng.next_u64())],
             &mut rng,
         );
@@ -168,81 +136,42 @@ mod e2e_transaction {
         let spent = balance_mob["spent"].as_str().unwrap();
         let secreted = balance_mob["secreted"].as_str().unwrap();
         let orphaned = balance_mob["orphaned"].as_str().unwrap();
-        assert_eq!(unspent, "600000000000000");
+        assert_eq!(unspent, (600 * MOB).to_string());
         assert_eq!(pending, "0");
         assert_eq!(spent, "0");
         assert_eq!(secreted, "0");
         assert_eq!(orphaned, "0");
 
-        // Get balance for each subaddress
-        let body = json!({
+        for (public_address, amount) in [
+            (&alice_b58_public_address, 100),
+            (&bob_b58_public_address, 200),
+            (&carol_b58_public_address, 300),
+        ]
+        .iter()
+        {
+            let body = json!({
             "jsonrpc": "2.0",
             "id": 1,
             "method": "get_address_status",
             "params": {
-                "address": alice_b58_public_address,
-            }
-        });
-        let res = dispatch(&client, body, &logger);
-        let result = res.get("result").unwrap();
-        let balance_per_token = result.get("balance_per_token").unwrap();
-        let balance_mob = balance_per_token.get(Mob::ID.to_string()).unwrap();
-        let unspent = balance_mob["unspent"].as_str().unwrap();
-        let pending = balance_mob["pending"].as_str().unwrap();
-        let spent = balance_mob["spent"].as_str().unwrap();
-        let secreted = balance_mob["secreted"].as_str().unwrap();
-        let orphaned = balance_mob["orphaned"].as_str().unwrap();
-        assert_eq!(unspent, (100 * MOB).to_string(),);
-        assert_eq!(pending, "0");
-        assert_eq!(spent, "0");
-        assert_eq!(secreted, "0");
-        assert_eq!(orphaned, "0");
-
-        let body = json!({
-            "jsonrpc": "2.0",
-            "id": 1,
-            "method": "get_address_status",
-            "params": {
-                "address": bob_b58_public_address,
-            }
-        });
-        let res = dispatch(&client, body, &logger);
-        let result = res.get("result").unwrap();
-        let balance_per_token = result.get("balance_per_token").unwrap();
-        let balance_mob = balance_per_token.get(Mob::ID.to_string()).unwrap();
-        let unspent = balance_mob["unspent"].as_str().unwrap();
-        let pending = balance_mob["pending"].as_str().unwrap();
-        let spent = balance_mob["spent"].as_str().unwrap();
-        let secreted = balance_mob["secreted"].as_str().unwrap();
-        let orphaned = balance_mob["orphaned"].as_str().unwrap();
-        assert_eq!(unspent, (200 * MOB).to_string(),);
-        assert_eq!(pending, "0");
-        assert_eq!(spent, "0");
-        assert_eq!(secreted, "0");
-        assert_eq!(orphaned, "0");
-
-        let body = json!({
-            "jsonrpc": "2.0",
-            "id": 1,
-            "method": "get_address_status",
-            "params": {
-                "address": carol_b58_public_address,
-            }
-        });
-        let res = dispatch(&client, body, &logger);
-        let result = res.get("result").unwrap();
-        let balance_per_token = result.get("balance_per_token").unwrap();
-        let balance_mob = balance_per_token.get(Mob::ID.to_string()).unwrap();
-        let unspent = balance_mob["unspent"].as_str().unwrap();
-        let pending = balance_mob["pending"].as_str().unwrap();
-        let spent = balance_mob["spent"].as_str().unwrap();
-        let secreted = balance_mob["secreted"].as_str().unwrap();
-        let orphaned = balance_mob["orphaned"].as_str().unwrap();
-        assert_eq!(unspent, (300 * MOB).to_string(),);
-        assert_eq!(pending, "0");
-        assert_eq!(spent, "0");
-        assert_eq!(secreted, "0");
-        assert_eq!(orphaned, "0");
+                "address": public_address,
+                }
+            });
+            let res = dispatch(&client, body, &logger);
+            let result = res.get("result").unwrap();
+            let balance_per_token = result.get("balance_per_token").unwrap();
+            let balance_mob = balance_per_token.get(Mob::ID.to_string()).unwrap();
+            let unspent = balance_mob["unspent"].as_str().unwrap();
+            let pending = balance_mob["pending"].as_str().unwrap();
+            let spent = balance_mob["spent"].as_str().unwrap();
+            let secreted = balance_mob["secreted"].as_str().unwrap();
+            let orphaned = balance_mob["orphaned"].as_str().unwrap();
+            assert_eq!(unspent, (amount * MOB).to_string(),);
+            assert_eq!(pending, "0");
+            assert_eq!(spent, "0");
+            assert_eq!(secreted, "0");
+            assert_eq!(orphaned, "0");
+        }
 
         // Imagine that Bob is sending 42.0 MOB to Alice
         let body = json!({
@@ -260,35 +189,6 @@ mod e2e_transaction {
         let result = res.get("result").unwrap();
         let tx_proposal: TxProposalJSON =
             serde_json::from_value(result.get("tx_proposal").unwrap().clone()).unwrap();
-        let transaction_log: TransactionLogJSON =
-            serde_json::from_value(result.get("transaction_log").unwrap().clone()).unwrap();
-
-        let tx_log_payload_txo = transaction_log.output_txos[0].clone();
-        let tx_proposal_payload_txo = tx_proposal.payload_txos[0].clone();
-        let tx_out_proto_bytes = hex::decode(tx_proposal_payload_txo.tx_out_proto).unwrap();
-        let txo: TxOut = mc_util_serial::decode(&tx_out_proto_bytes).unwrap();
-
-        let txo_public_key_bytes = hex::decode(tx_log_payload_txo.public_key).unwrap();
-        let txo_public_key: CompressedRistrettoPublic =
-            txo_public_key_bytes.as_slice().try_into().unwrap();
-
-        assert_eq!(txo.public_key, txo_public_key);
-        assert_eq!(
-            tx_proposal.fee_amount,
-            AmountJSON::new(Mob::MINIMUM_FEE, Mob::ID)
-        );
-        assert_eq!(tx_proposal.input_txos.len(), 1);
-        assert_eq!(tx_proposal.payload_txos.len(), 1);
-        // One change (and that change should be going back to Bob)
-        assert_eq!(tx_proposal.change_txos.len(), 1);
-        assert_eq!(
-            tx_proposal.change_txos[0].recipient_public_address_b58,
-            bob_b58_public_address
-        );
-        // Tombstone block = ledger height (12 to start + 3 new blocks + 10 default
-        // tombstone)
-        assert_eq!(tx_proposal.tombstone_block_index, "25");
-
         let payments_tx_proposal = TxProposal::try_from(&tx_proposal).unwrap();
 
         add_block_with_tx(&mut ledger_db, payments_tx_proposal.tx, &mut rng);
@@ -330,7 +230,7 @@ mod e2e_transaction {
         // The SPENT value is calculated by the txos that have key_images that have been
         // burned. It does not take into account change. Bob's 200 MOB TXO was
         // burned to send 42 MOB to Alice.
-        assert_eq!(spent, (200 * MOB).to_string()); //FIXME: then shouldn't this be 200?
+        assert_eq!(spent, (200 * MOB).to_string());
         assert_eq!(secreted, "0");
         assert_eq!(orphaned, "0");
 
@@ -352,11 +252,9 @@ mod e2e_transaction {
         let spent = balance_mob["spent"].as_str().unwrap();
         let secreted = balance_mob["secreted"].as_str().unwrap();
         let orphaned = balance_mob["orphaned"].as_str().unwrap();
-        assert_eq!(unspent, (142 * MOB).to_string(),); // FIXME: shouldn't this be 142?
+        assert_eq!(unspent, (142 * MOB).to_string(),);
         assert_eq!(pending, "0");
-        assert_eq!(spent, (0).to_string(),); // FIXME: Why was it spent from Alice's balance?
-                                             //assert_eq!(spent, (100 * MOB).to_string(),); // FIXME: Why was it spent from
-                                             // Alice's balance?
+        assert_eq!(spent, (0).to_string(),);
         assert_eq!(secreted, "0");
         assert_eq!(orphaned, "0");
 
