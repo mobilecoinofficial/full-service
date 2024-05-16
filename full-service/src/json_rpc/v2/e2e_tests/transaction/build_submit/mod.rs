8817ec205142a5e7d60b6e71a4cd2a771b4e6e4a
sugargoat <sugargoat@mobilecoin.com>
2024-05-16 08:53:00 -0700

    Rename complete
    Signed-off-by: sugargoat <sugargoat@mobilecoin.com>

--- full-service/src/json_rpc/v2/e2e_tests/transaction/build_submit/mod.rs
+++ full-service/src/json_rpc/v2/e2e_tests/transaction/build_submit/mod.rs
@@ -1,5 +1,5 @@
 mod build_and_submit;
-mod build_and_submit_spend_from_subaddress;
+mod build_and_submit_with_subaddress_to_spend_from;
 mod build_then_submit;
 mod build_unsigned;
 mod large_transaction;
