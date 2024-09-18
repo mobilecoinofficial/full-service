34f8e0409d933900c7acc62782e2eff3acc4b3df
Nick Santana <nick@mobilecoin.com>
2024-09-18 15:38:33 -0700

    I already said I was sorry
    have the building of the transaction

---
+++ spam_tombestone.py
@@ -0,0 +1,30 @@
+import requests
+
+FS_URL = 'http://localhost:9090/wallet/v2'
+
+def fs_request(method, params=None):
+    json = {"method": method, "jsonrpc":"2.0", "id":1}
+    if params:
+        json['params'] = params
+    return requests.post(FS_URL, json={"method": method, "jsonrpc":"2.0", "id":1, "params": params}).json()
+
+def get_block_height():
+    response = fs_request('get_network_status')
+    return response['result']['network_status']['network_block_height']
+
+def build_transaction():
+    block = int(get_block_height())
+    params = {"account_id": "353a142530426bed601805ef0da8412dd323797bd07614202b8acdd0a249a1c5",
+              "recipient_public_address": "2hQXjtgFSQ7XjAu1JYtBwUjJDZEBFr94FQv6f8ELDBr9BU2ngoug6WoM36naNMwsx87mkuw97xCCysTcRKQ5q5DNfzT5hQ3V724m57Mzx42",
+              "amount": {"value": "000001", "token_id": "1"},
+              "tombstone_block": str(block + 1)
+              }
+    response = fs_request('build_transaction', params)
+    return response
+
+def main():
+    response = build_transaction()
+    print(f"the response is {response}")
+
+if __name__ == '__main__':
+    main()
